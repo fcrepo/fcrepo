30a6f20522ec9dd16ce6f73a658c7152401f2489
Justin Coyne <justin.coyne@yourmediashelf.com>
2013-02-13 12:22:25 -0600

    Fixed fedora_object.rb

--- fcrepo-admin/src/fedora-admin/app/models/fedora_object.rb
+++ fcrepo-admin/src/fedora-admin/app/models/fedora_object.rb
@@ -1,18 +1,19 @@
 class FedoraObject
 
+  extend ActiveModel::Naming
+
+  attr_reader :name, :label, :path, :owner, :created, :modified
+
   def initialize(java_object)
-    @java_object = java_object
+    @name = java_object.getName.to_s
+    @label = @name
+    @path = java_object.getPath.to_s
+    @owner = java_object.getProperty("fedora:ownerId").getString
+    @created = java_object.getProperty("jcr:created").getString
+    @modified = java_object.getProperty("jcr:lastModified").getString
   end
 
-  def name
-    @java_object.getName.to_s
-  end
-
-  def path
-    @java_object.getPath.to_s
-  end
-
-  def model_name
-    'fedora_object'
+  def to_param
+    name
   end
 end
