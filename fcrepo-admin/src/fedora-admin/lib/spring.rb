36f1477bf1850cf9ac291c4a5acd0f66e8f68e93
Justin Coyne <justin.coyne@yourmediashelf.com>
2013-02-13 13:20:25 -0600

    Only stop spring if it's been started

--- fcrepo-admin/src/fedora-admin/lib/spring.rb
+++ fcrepo-admin/src/fedora-admin/lib/spring.rb
@@ -4,20 +4,20 @@ class Spring
   include Singleton
 
   def get(bean_name)
-    @repo = @ctx.getBean("modeshapeRepofactory")
+    @repo = context.getBean("modeshapeRepofactory")
   end
  
   def stop
+    return unless @ctx
     $stderr.puts "Stopping spring..."
     @ctx.destroy
     @ctx.close
     $stderr.puts "Stopped"
   end
 
-  def initialize
+  def context
     spring_file = "config/repo.xml"
-    puts "Spring #{spring_file}"
-    @ctx = org.springframework.context.support.ClassPathXmlApplicationContext.new(spring_file)
+    @ctx ||= org.springframework.context.support.ClassPathXmlApplicationContext.new(spring_file)
   end
 end
 
