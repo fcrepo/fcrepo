f914d7c15d5d1e98c7fded7b4b7b8de95137ef38
Justin Coyne <justin.coyne@yourmediashelf.com>
2013-02-13 14:16:45 -0600

    Added list of datastreams

--- fcrepo-admin/src/fedora-admin/app/models/datastream.rb
+++ fcrepo-admin/src/fedora-admin/app/models/datastream.rb
@@ -1,7 +1,15 @@
 class Datastream
 
+  extend ActiveModel::Naming
+
+  attr_reader :name
+
   def initialize(name)
     @name = name
+  end
+
+  def to_param
+    name
   end
 
 end
--- fcrepo-admin/src/fedora-admin/config/routes.rb
+++ fcrepo-admin/src/fedora-admin/config/routes.rb
@@ -1,5 +1,7 @@
 FedoraAdmin::Application.routes.draw do
   root :to => 'admin#index'
 
-  resources :fedora_objects, :controller=>'admin', :path=>'objects'
+  resources :fedora_objects, :controller=>'admin', :path=>'objects' do
+    resources :datastreams
+  end
 end
