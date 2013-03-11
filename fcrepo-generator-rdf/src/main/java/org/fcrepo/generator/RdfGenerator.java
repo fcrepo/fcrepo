58f022e99956c5c5b8c855e24e097eb8e16aa435
ajs6f <ajs6f@virginia.edu>
2013-03-11 15:32:02 -0400

    Switiching to Any23 for RDF serialization for: https://www.pivotaltracker.com/story/show/45864223

--- fcrepo-generator-rdf/src/main/java/org/fcrepo/generator/RdfGenerator.java
+++ fcrepo-generator-rdf/src/main/java/org/fcrepo/generator/RdfGenerator.java
@@ -1,62 +1,123 @@
+
 package org.fcrepo.generator;
 
-
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 import static javax.ws.rs.core.MediaType.TEXT_XML;
-import static javax.ws.rs.core.Response.ok;
-import static org.fcrepo.services.ObjectService.getObjectNode;
+import static org.fcrepo.services.ObjectService.getObject;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 
-import javax.annotation.Resource;
-import javax.jcr.Node;
+import javax.jcr.NamespaceRegistry;
 import javax.jcr.RepositoryException;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
 
-import org.fcrepo.generator.rdf.TripleGenerator;
-
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.RDFWriter;
+import org.apache.any23.extractor.ExtractionContext;
+import org.apache.any23.writer.NTriplesWriter;
+import org.apache.any23.writer.RDFXMLWriter;
+import org.apache.any23.writer.TripleHandler;
+import org.apache.any23.writer.TripleHandlerException;
+import org.apache.any23.writer.TurtleWriter;
+import org.fcrepo.AbstractResource;
+import org.fcrepo.FedoraObject;
+import org.fcrepo.generator.rdf.TripleSource;
+import org.fcrepo.generator.rdf.TripleSource.Triple;
+import org.openrdf.model.Literal;
+import org.openrdf.model.URI;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.sail.memory.model.MemValueFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/objects/{pid}/rdf")
-public class RdfGenerator {
+@Produces({TEXT_XML, TEXT_PLAIN})
+public class RdfGenerator extends AbstractResource {
 
-    @Resource
-    List<TripleGenerator> rdfgenerators;
+    List<TripleSource<FedoraObject>> objectGenerators;
+
+    final private static ValueFactory valFactory = new MemValueFactory();
+
+    final private Logger logger = LoggerFactory.getLogger(RdfGenerator.class);
 
     @GET
-    @Produces(TEXT_XML)
-    public Response getObjectAsRdfXml(@PathParam("pid")
-                                          final String pid) throws RepositoryException {
+    @Path("/")
+    @Produces({TEXT_XML, "text/turtle", TEXT_PLAIN})
+    public String getRdfXml(@PathParam("pid")
+    final String pid, @HeaderParam("Accept")
+    @DefaultValue(TEXT_XML)
+    final String mimeType) throws IOException, RepositoryException,
+            TripleHandlerException {
 
-        final Node obj = getObjectNode(pid);
+        final FedoraObject obj = getObject(pid);
+        final URI docURI = valFactory.createURI("info:" + pid);
+        logger.debug("Using ValueFactory: " + valFactory.toString());
+        final ExtractionContext context =
+                new ExtractionContext("Fedora Serialization Context", docURI);
+        logger.debug("Using ExtractionContext: " + context.toString());
 
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
 
-        final Model model = getObjectAsRdf(obj);
+            // possible serializations
+            TripleHandler writer;
+            switch (mimeType) {
+                case "text/turtle":
+                    writer = new TurtleWriter(out);
+                    break;
+                case TEXT_PLAIN:
+                    writer = new NTriplesWriter(out);
+                default:
+                    writer = new RDFXMLWriter(out);
+                    break;
+            }
 
-        RDFWriter w = model.getWriter("RDF/XML");
+            logger.trace("Created RDF Writer: " + writer.getClass().getName());
 
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        w.write(model, os, "RDF/XML");
+            writer.openContext(context);
+            writer.startDocument(docURI);
+            // add JCR-managed namespaces
+            final NamespaceRegistry nReg =
+                    obj.getNode().getSession().getWorkspace()
+                            .getNamespaceRegistry();
+            for (final String prefix : nReg.getPrefixes()) {
+                writer.receiveNamespace(prefix, nReg.getURI(prefix), context);
+            }
 
-        return ok(os.toString()).build();
-    }
+            for (final TripleSource<FedoraObject> tripleSource : objectGenerators) {
+                logger.trace("Using TripleSource: " +
+                        tripleSource.getClass().getName());
+                for (final Triple t : tripleSource.getTriples(obj, uriInfo)) {
+                    logger.debug("TripleSource generated: " + t.toString());
+                    final URI subject = valFactory.createURI(t.subject);
+                    logger.trace("Created final triple subject: " +
+                            subject.toString());
+                    final URI predicate = valFactory.createURI(t.predicate);
+                    logger.trace("Created final triple predicate: " +
+                            predicate.toString());
+                    final Literal object = valFactory.createLiteral(t.object);
+                    logger.trace("Created final triple object: " +
+                            object.toString());
+                    writer.receiveTriple(subject, predicate, object, null,
+                            context);
+                }
+            }
 
-    private Model getObjectAsRdf(final Node obj) throws RepositoryException {
-
-        final Model model = ModelFactory.createDefaultModel();
-
-        final com.hp.hpl.jena.rdf.model.Resource resource = model.createResource(obj.getIdentifier());
-        for (TripleGenerator indexer : rdfgenerators) {
-            indexer.updateResourceFromNode(resource, obj);
+            writer.endDocument(docURI);
+            writer.close();
+            logger.debug("Generated RDF: " + out.toString());
+            return out.toString();
         }
 
-        return model;
+    }
+
+    public void setObjectGenerators(
+            List<TripleSource<FedoraObject>> objectGenerators) {
+        this.objectGenerators = objectGenerators;
     }
 
 }
--- fcrepo-generator-rdf/src/test/java/org/fcrepo/generator/RdfGeneratorTest.java
+++ fcrepo-generator-rdf/src/test/java/org/fcrepo/generator/RdfGeneratorTest.java
@@ -1,5 +1,5 @@
-package org.fcrepo.generator;
 
+package org.fcrepo.generator;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -17,8 +17,10 @@ import static org.junit.Assert.assertTrue;
 public class RdfGeneratorTest extends AbstractResourceTest {
 
     @Test
-    public void testJcrPropertiesBasedTriples() throws Exception {
-        getStatus(postObjMethod("RdfTest1"));
+    public void testXMLObjectTriples() throws Exception {
+
+        logger.debug("Executing testXMLObjectTriples()...");
+        client.execute(postObjMethod("RdfTest1"));
 
         HttpGet getRdfMethod =
                 new HttpGet(serverAddress + "objects/RdfTest1/rdf");
@@ -27,10 +29,49 @@ public class RdfGeneratorTest extends AbstractResourceTest {
         assertEquals(200, response.getStatusLine().getStatusCode());
 
         final String content = EntityUtils.toString(response.getEntity());
-        assertTrue("Didn't find purl!", compile("purl", DOTALL).matcher(
-                content).find());
 
-        assertTrue("Didn't find identifier!", compile("identifier",
-                DOTALL).matcher(content).find());
+        assertTrue("Didn't find identifier!", compile("identifier", DOTALL)
+                .matcher(content).find());
+        logger.debug("Finished testXMLObjectTriples().");
+
     }
+
+    @Test
+    public void testNTriplesObjectTriples() throws Exception {
+        logger.debug("Executing testNTriplesObjectTriples()...");
+
+        client.execute(postObjMethod("RdfTest2"));
+
+        HttpGet getRdfMethod =
+                new HttpGet(serverAddress + "objects/RdfTest2/rdf");
+        HttpResponse response = client.execute(getRdfMethod);
+        assertEquals(200, response.getStatusLine().getStatusCode());
+
+        final String content = EntityUtils.toString(response.getEntity());
+
+        assertTrue("Didn't find identifier!", compile("identifier", DOTALL)
+                .matcher(content).find());
+        logger.debug("Finished testNTriplesObjectTriples().");
+    }
+
+    @Test
+    public void testTurtleObjectTriples() throws Exception {
+        logger.debug("Executing testTurtleObjectTriples()...");
+        client.execute(postObjMethod("RdfTest3"));
+
+        HttpGet getRdfMethod =
+                new HttpGet(serverAddress + "objects/RdfTest3/rdf");
+        getRdfMethod.setHeader("Accept", "text/turtle");
+
+        HttpResponse response = client.execute(getRdfMethod);
+        assertEquals(200, response.getStatusLine().getStatusCode());
+
+        final String content = EntityUtils.toString(response.getEntity());
+
+        assertTrue("Didn't find identifier!", compile("identifier", DOTALL)
+                .matcher(content).find());
+
+        logger.debug("Finished testTurtleObjectTriples().");
+    }
+
 }
