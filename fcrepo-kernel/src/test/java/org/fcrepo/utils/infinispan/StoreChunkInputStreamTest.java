17784a7df045ab61d5293c9500c1d1d4ed21a372
Edwin Shin <edwin.shin@yourmediashelf.com>
2013-03-16 02:27:11 +0800

    Removes unused code. Cleans up imports.

--- fcrepo-kernel/src/test/java/org/fcrepo/utils/infinispan/StoreChunkInputStreamTest.java
+++ fcrepo-kernel/src/test/java/org/fcrepo/utils/infinispan/StoreChunkInputStreamTest.java
@@ -1,5 +1,10 @@
 package org.fcrepo.utils.infinispan;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.apache.commons.io.IOUtils;
 import org.infinispan.Cache;
 import org.infinispan.loaders.CacheLoaderManager;
@@ -7,24 +12,14 @@ import org.infinispan.loaders.CacheStore;
 import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.junit.Test;
-import org.modeshape.jcr.value.binary.BinaryStore;
-import org.modeshape.jcr.value.binary.infinispan.InfinispanBinaryStore;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Set;
-
-import static org.junit.Assert.assertEquals;
 
 public class StoreChunkInputStreamTest {
 
     @Test
     public void tryRetrievingContentFromInfinispanTest() throws IOException {
         EmbeddedCacheManager cm = new DefaultCacheManager("test_infinispan_configuration.xml");
-        BinaryStore store = new InfinispanBinaryStore(cm, false, "FedoraRepository", "FedoraRepository");
 
         Cache<String, byte[]> ispn = cm.getCache("FedoraRepository");
-
 
         CacheStore cs = ispn.getAdvancedCache().getComponentRegistry()
                 .getComponent(CacheLoaderManager.class)
@@ -37,8 +32,6 @@ public class StoreChunkInputStreamTest {
         ispn.put("key-data-4", "4".getBytes());
         ispn.put("key-data-5", "5".getBytes());
 
-
-        Set s = ispn.keySet();
         InputStream is = new StoreChunkInputStream(cs, "key-data");
 
         String data = IOUtils.toString(is);
