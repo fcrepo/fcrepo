abfbb5b6b9d352bc0ae922aa242bfee4f32cb963
Chris Beer <cabeer@stanford.edu>
2013-03-30 09:20:26 -0700

    metrics-SNAPSHOT removed Reporter interface

--- fcrepo-metrics/src/main/java/org/fcrepo/metrics/ReporterFactory.java
+++ fcrepo-metrics/src/main/java/org/fcrepo/metrics/ReporterFactory.java
@@ -1,8 +1,8 @@
 package org.fcrepo.metrics;
 
+import com.yammer.metrics.AbstractPollingReporter;
 import com.yammer.metrics.Clock;
 import com.yammer.metrics.MetricFilter;
-import com.yammer.metrics.Reporter;
 import com.yammer.metrics.graphite.Graphite;
 import com.yammer.metrics.graphite.GraphiteReporter;
 
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 
 public class ReporterFactory {
 
-    public Reporter registerGraphiteReporter(Graphite g, String prefix) {
+    public AbstractPollingReporter registerGraphiteReporter(Graphite g, String prefix) {
         GraphiteReporter r = new GraphiteReporter(RegistryService.getMetrics(), g, Clock.defaultClock(), prefix, TimeUnit.SECONDS, TimeUnit.MILLISECONDS, MetricFilter.ALL);
 
         r.start(1, TimeUnit.MINUTES);
