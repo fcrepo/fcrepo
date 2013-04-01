108eb1b7a4137faef54637c023cc178225354c79
Chris Beer <cabeer@stanford.edu>
2013-04-01 16:27:11 -0700

    pin to metrics 3.0.0.BETA1

--- fcrepo-metrics/src/main/java/org/fcrepo/metrics/ReporterFactory.java
+++ fcrepo-metrics/src/main/java/org/fcrepo/metrics/ReporterFactory.java
@@ -1,6 +1,6 @@
 package org.fcrepo.metrics;
 
-import com.yammer.metrics.AbstractPollingReporter;
+import com.yammer.metrics.ScheduledReporter;
 import com.yammer.metrics.Clock;
 import com.yammer.metrics.MetricFilter;
 import com.yammer.metrics.graphite.Graphite;
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 
 public class ReporterFactory {
 
-    public AbstractPollingReporter registerGraphiteReporter(Graphite g, String prefix) {
+    public ScheduledReporter registerGraphiteReporter(Graphite g, String prefix) {
         GraphiteReporter r = GraphiteReporter.forRegistry(RegistryService.getMetrics()).build(g);
 
         r.start(1, TimeUnit.MINUTES);
