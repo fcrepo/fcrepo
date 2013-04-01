df8b211a6c56d828f6a50445cb414f74b337bc30
Edwin Shin <edwin.shin@yourmediashelf.com>
2013-04-01 23:52:59 +0900

    46822943: Unit test frepo-http-api/FedoraIdentifiers
    Unit test for FedoraIdentifiers
    
    https://www.pivotaltracker.com/story/show/46822943

---
+++ fcrepo-http-api/src/test/java/org/fcrepo/api/FedoraIdentifiersTest.java
@@ -0,0 +1,44 @@
+package org.fcrepo.api;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import org.fcrepo.identifiers.PidMinter;
+import org.fcrepo.jaxb.responses.management.NextPid;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.google.common.base.Function;
+
+public class FedoraIdentifiersTest {
+
+    @Mock
+    private PidMinter mockPidMinter;
+
+    @InjectMocks
+    private FedoraIdentifiers fi = new FedoraIdentifiers();
+
+    @Before
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testGetNextPid() {
+        when(mockPidMinter.makePid()).thenReturn(new Function<Object, String>() {
+
+            @Override
+            public String apply(Object input) {
+                return "asdf:123";
+            }
+                });
+
+        NextPid np = fi.getNextPid(2);
+
+        //TODO: test for actual values
+        assertNotNull(np);
+    }
+}
