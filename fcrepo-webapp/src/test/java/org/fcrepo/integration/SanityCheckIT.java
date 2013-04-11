79658a68bbb2902f003a7d6c2385923ab4849cdc
Edwin Shin <edwin.shin@yourmediashelf.com>
2013-04-11 22:18:25 +0800

    Uses build-helper-maven-plugin to reserve unused ports, which are then used in the jetty-maven-plugin configuration and made available to test classes via the system property "test.port".

--- fcrepo-webapp/src/test/java/org/fcrepo/integration/SanityCheckIT.java
+++ fcrepo-webapp/src/test/java/org/fcrepo/integration/SanityCheckIT.java
@@ -1,6 +1,11 @@
 package org.fcrepo.integration;
 
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -15,14 +20,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
-import static org.junit.Assert.assertEquals;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"/spring/repo.xml", "/spring/eventing.xml", "/spring/jms.xml", "/spring/generator.xml", "/spring-test/rest.xml"})
 public class SanityCheckIT {
+
+    /**
+     * The server port of the application, set as system property by 
+     * maven-failsafe-plugin.
+     */
+    private static final String SERVER_PORT = System.getProperty("test.port");
+
+    /**
+    * The context path of the application (including the leading "/"), set as 
+    * system property by maven-failsafe-plugin.
+    */
+    private static final String CONTEXT_PATH = System
+            .getProperty("test.context.path");
 
     protected Logger logger;
 
@@ -31,13 +44,10 @@ public class SanityCheckIT {
         logger = LoggerFactory.getLogger(this.getClass());
     }
 
-    protected static final int SERVER_PORT = Integer.parseInt(System
-            .getProperty("test.port", "8080"));
-
     protected static final String HOSTNAME = "localhost";
 
     protected static final String serverAddress = "http://" + HOSTNAME + ":" +
-            SERVER_PORT + "/rest/";
+            SERVER_PORT + CONTEXT_PATH;
 
     protected static final PoolingClientConnectionManager connectionManager =
             new PoolingClientConnectionManager();
@@ -54,7 +64,7 @@ public class SanityCheckIT {
     @Test
     public void doASanityCheck() throws IOException {
         assertEquals(200, getStatus(new HttpGet(serverAddress +
-                "describe")));
+ "/describe")));
     }
 
 
