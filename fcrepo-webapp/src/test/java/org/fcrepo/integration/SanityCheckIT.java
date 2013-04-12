dff8f8b04ec73f8f75cc49444133869fb7bf2caf
Edwin Shin <edwin.shin@yourmediashelf.com>
2013-04-12 12:38:26 +0800

    Removes SpringJunit4ClassRunner from SanityCheckIT in fcrepo-webapp since we're relying on jetty-maven-plugin to start/stop the container for testing.

--- fcrepo-webapp/src/test/java/org/fcrepo/integration/SanityCheckIT.java
+++ fcrepo-webapp/src/test/java/org/fcrepo/integration/SanityCheckIT.java
@@ -14,14 +14,9 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.PoolingClientConnectionManager;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"/spring/repo.xml", "/spring/eventing.xml", "/spring/jms.xml", "/spring/generator.xml", "/spring-test/rest.xml"})
 public class SanityCheckIT {
 
     /**
