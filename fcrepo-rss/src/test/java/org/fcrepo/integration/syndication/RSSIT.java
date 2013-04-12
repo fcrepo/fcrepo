53c586f18b7e847d953decb603c59a337cce35c8
ajs6f <ajs6f@virginia.edu>
2013-04-12 11:17:53 -0400

    Removed dependency on HTTP APIs for testing per: https://www.pivotaltracker.com/story/show/47962441

--- fcrepo-rss/src/test/java/org/fcrepo/integration/syndication/RSSIT.java
+++ fcrepo-rss/src/test/java/org/fcrepo/integration/syndication/RSSIT.java
@@ -3,14 +3,24 @@ package org.fcrepo.integration.syndication;
 
 import static java.util.regex.Pattern.DOTALL;
 import static java.util.regex.Pattern.compile;
+import static org.fcrepo.services.PathService.OBJECT_PATH;
+import static org.fcrepo.utils.FedoraJcrTypes.FEDORA_OBJECT;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import javax.inject.Inject;
+import javax.jcr.Node;
+import javax.jcr.Repository;
+import javax.jcr.Session;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.util.EntityUtils;
+import org.fcrepo.services.PathService;
+import org.fcrepo.utils.FedoraJcrTypes;
 import org.junit.Test;
+import org.modeshape.jcr.api.JcrTools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
@@ -20,12 +30,21 @@ import org.springframework.test.context.ContextConfiguration;
 public class RSSIT extends AbstractResourceIT {
 
     final private Logger logger = LoggerFactory.getLogger(RSSIT.class);
-
+    
+    @Inject
+    Repository repo;
+    
+    JcrTools jcrTools = new JcrTools(true);
+    
     @Test
     public void testRSS() throws Exception {
 
-        assertEquals(201, getStatus(new HttpPost(serverAddress +
-                "/objects/RSSTESTPID")));
+        final Session session = repo.login();
+        final Node object =
+                jcrTools.findOrCreateChild(session.getNode(OBJECT_PATH), "RSSTESTPID");
+        object.addMixin(FEDORA_OBJECT);
+        session.save();
+        session.logout();
 
         HttpGet getRSSMethod = new HttpGet(serverAddress + "/rss");
         HttpResponse response = client.execute(getRSSMethod);
