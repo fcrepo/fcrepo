888e63c4df8ef993bade131ec959cc4795176e68
Vincent Nguyen <vnguyen745@yahoo.com>
2013-04-25 11:48:39 -0400

    fixed integration test for describe request

--- fcrepo-http-api/src/test/java/org/fcrepo/integration/api/FedoraRepositoryIT.java
+++ fcrepo-http-api/src/test/java/org/fcrepo/integration/api/FedoraRepositoryIT.java
@@ -36,7 +36,7 @@ public class FedoraRepositoryIT extends AbstractResourceIT {
         final String description = EntityUtils.toString(response.getEntity());
         logger.debug("Found the repository description:\n" + description);
         assertTrue("Failed to find a proper repo version", compile(
-                "<repositoryVersion>.*?</repositoryVersion>").matcher(
+                "<.*repositoryVersion>.*?<.*repositoryVersion>").matcher(
                 description).find());
     }
 
@@ -61,7 +61,7 @@ public class FedoraRepositoryIT extends AbstractResourceIT {
         final String description = EntityUtils.toString(response.getEntity());
         logger.debug("Found a repository description:\n" + description);
         final Matcher check =
-                compile("<repositorySize>([0-9]+)</repositorySize>", DOTALL)
+                compile("<.*repositorySize>([0-9]+)<.*repositorySize>", DOTALL)
                         .matcher(description);
         Long oldSize = null;
         while (check.find()) {
@@ -80,7 +80,7 @@ public class FedoraRepositoryIT extends AbstractResourceIT {
                 EntityUtils.toString(response.getEntity());
         logger.debug("Found another repository description:\n" + newDescription);
         final Matcher newCheck =
-                compile("<repositorySize>([0-9]+)</repositorySize>", DOTALL)
+                compile("<.*repositorySize>([0-9]+)<.*repositorySize>", DOTALL)
                         .matcher(newDescription);
         Long newSize = null;
         while (newCheck.find()) {
