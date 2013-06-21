ba9cebff911ab57e6b0fc2d5453940bd99e22b66
Edwin Shin <edwin.shin@yourmediashelf.com>
2013-06-22 04:03:39 +0800

    minor: adds generated serialVersionUID

--- fcrepo-kernel/src/main/java/org/fcrepo/exception/TransactionMissingException.java
+++ fcrepo-kernel/src/main/java/org/fcrepo/exception/TransactionMissingException.java
@@ -4,6 +4,8 @@ import javax.jcr.RepositoryException;
 
 public class TransactionMissingException extends RepositoryException {
 
+    private static final long serialVersionUID = 2139084821001303830L;
+
     public TransactionMissingException(final String s) {
         super(s);
     }
