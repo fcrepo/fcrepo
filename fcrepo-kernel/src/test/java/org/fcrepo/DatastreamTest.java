e7be62359542991ecfeba2ed0e938fec2a9f8c26
Edwin Shin <edwin.shin@yourmediashelf.com>
2013-06-27 03:42:04 +0800

    Adds license headers via mvn license:format

--- fcrepo-kernel/src/test/java/org/fcrepo/DatastreamTest.java
+++ fcrepo-kernel/src/test/java/org/fcrepo/DatastreamTest.java
@@ -1,9 +1,18 @@
 /**
- * The contents of this file are subject to the license and copyright terms
- * detailed in the license directory at the root of the source tree (also
- * available online at http://fedora-commons.org/license/).
+ * Copyright 2013 DuraSpace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package org.fcrepo;
 
 import static org.junit.Assert.assertEquals;
