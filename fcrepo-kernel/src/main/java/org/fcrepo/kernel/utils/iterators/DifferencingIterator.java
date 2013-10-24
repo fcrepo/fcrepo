c825b32864dfa3710edc2737df33657fe87eb9da
ajs6f <ajs6f@virginia.edu>
2013-10-24 11:28:45 -0400

    Fixing checkstyle problems

--- fcrepo-kernel/src/main/java/org/fcrepo/kernel/utils/iterators/DifferencingIterator.java
+++ fcrepo-kernel/src/main/java/org/fcrepo/kernel/utils/iterators/DifferencingIterator.java
@@ -1,9 +1,23 @@
+/**
+ * Copyright 2013 DuraSpace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.fcrepo.kernel.utils.iterators;
 
 import java.util.Iterator;
 import java.util.Queue;
-import java.util.Set;
 
 import com.google.common.base.Predicate;
 
@@ -26,12 +40,21 @@ public class DifferencingIterator<T> {
 
     private Iterator<T> source;
 
+    /**
+     * Default constructor.
+     *
+     * @param toBeMatched
+     * @param source
+     */
     public DifferencingIterator(final Predicate<T> toBeMatched,
             final Iterator<T> source) {
         this.toBeMatched = toBeMatched;
         this.source = source;
     }
 
+    /**
+     * Executes the differencing.
+     */
     public void divide() {
         while (source.hasNext()) {
             final T next = source.next();
