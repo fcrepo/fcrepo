0f117dd64fdbb66e9c7ce1a3e01c4aa3f18d9d90
ajs6f <ajs6f@virginia.edu>
2013-11-24 10:27:46 -0500

    Better equals and hashCode for RdfStream

--- fcrepo-kernel/src/main/java/org/fcrepo/kernel/utils/iterators/RdfStream.java
+++ fcrepo-kernel/src/main/java/org/fcrepo/kernel/utils/iterators/RdfStream.java
@@ -16,14 +16,16 @@
 
 package org.fcrepo.kernel.utils.iterators;
 
+import static com.google.common.base.Objects.equal;
 import static com.google.common.collect.ImmutableSet.copyOf;
 import static com.google.common.collect.Iterators.singletonIterator;
 import static com.hp.hpl.jena.rdf.model.ModelFactory.createDefaultModel;
+import static java.util.Objects.hash;
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
-
 import javax.jcr.Session;
 
 import com.google.common.base.Function;
@@ -350,22 +352,37 @@ public class RdfStream extends ForwardingIterator<Triple> implements
      */
     @Override
     public boolean equals(final Object o) {
-        if (o instanceof RdfStream) {
-            final RdfStream rdfo = (RdfStream) o;
-            final boolean triplesEqual =
-                copyOf(rdfo.triples).equals(copyOf(this.triples));
-            final boolean namespaceMappingsEqual =
-                rdfo.namespaces().equals(this.namespaces());
-            return triplesEqual && namespaceMappingsEqual;
-        } else {
+        if (o == null) {
             return false;
         }
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof RdfStream)) {
+            return false;
+        }
+        final RdfStream rdfo = (RdfStream) o;
+
+        final boolean triplesEqual =
+            equal(copyOf(rdfo.triples), copyOf(this.triples));
+
+        final boolean namespaceMappingsEqual =
+            equal(rdfo.namespaces(), this.namespaces());
+
+        final boolean sessionEqual =
+                equal(rdfo.session(), this.session());
+
+        final boolean topicEqual =
+                equal(rdfo.topic(), this.topic());
+
+        return triplesEqual && namespaceMappingsEqual && sessionEqual
+                && topicEqual;
+
     }
 
     @Override
     public int hashCode() {
-        return namespaces().hashCode() + 2 * triples.hashCode() + 3
-                * context.hashCode() + 5 * topic.hashCode();
+        return hash(namespaces(), triples, session(), topic());
     }
 
 }
