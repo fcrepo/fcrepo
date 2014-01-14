4692be2ff3a5726d33e4f4ac83161bc6f208d6cd
fasseg <frank.asseg@googlemail.com>
2014-01-14 14:40:06 +0100

    added ExceptionMapper for AccessDeniedException

---
+++ fcrepo-http-commons/src/main/java/org/fcrepo/http/commons/exceptionhandlers/AccessDeniedExceptionMapper.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2013 DuraSpace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.fcrepo.http.commons.exceptionhandlers;
+
+import static javax.ws.rs.core.Response.status;
+import static javax.ws.rs.core.Response.Status.FORBIDDEN;
+
+import javax.jcr.AccessDeniedException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AccessDeniedExceptionMapper implements
+        ExceptionMapper<AccessDeniedException> {
+
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(AccessDeniedExceptionMapper.class);
+
+    /*
+     * (non-Javadoc)
+     * @see javax.ws.rs.ext.ExceptionMapper#toResponse(java.lang.Throwable)
+     */
+    @Override
+    public Response toResponse(AccessDeniedException e) {
+        LOGGER.debug("{} intercepted exception:{} \n", this.getClass()
+                .getSimpleName(), e);
+        return status(FORBIDDEN).build();
+    }
+}
