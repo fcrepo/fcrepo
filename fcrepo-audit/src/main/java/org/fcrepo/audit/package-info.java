d37644f5519345b896bf54680fb5bbd8b503034c
Mike Durbin <md5wz@virginia.edu>
2014-05-05 14:31:27 -0400

    Updated license copyright year to 2014.

--- fcrepo-audit/src/main/java/org/fcrepo/audit/package-info.java
+++ fcrepo-audit/src/main/java/org/fcrepo/audit/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013 DuraSpace, Inc.
+ * Copyright 2014 DuraSpace, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  * This package provides functionality for auditing Fedora events. An <code> Auditor</code> records auditable Fedora
  *  events.
