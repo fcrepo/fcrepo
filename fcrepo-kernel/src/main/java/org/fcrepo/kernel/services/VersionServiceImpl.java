4887f221e96f2305cd58a8945cc8136c699d0fa7
Mike Durbin <md5wz@virginia.edu>
2014-05-07 16:42:51 -0400

    Updated VersionServiceImpl to add mix:versionable when user attempts to create versions.

--- fcrepo-kernel/src/main/java/org/fcrepo/kernel/services/VersionServiceImpl.java
+++ fcrepo-kernel/src/main/java/org/fcrepo/kernel/services/VersionServiceImpl.java
@@ -87,8 +87,10 @@ public class VersionServiceImpl extends AbstractService implements VersionServic
      */
     @Override
     public void nodeUpdated(final Node n) throws RepositoryException {
-        if (isVersioningEnabled(n)
-                && isImplicitVersioningEnabled(n)) {
+        if (isImplicitVersioningEnabled(n)) {
+            if (!isVersioningEnabled(n)) {
+                enableVersioning(n);
+            }
             queueOrCommitCheckpoint(n.getSession(), n.getPath());
         } else {
             LOGGER.trace("No implicit version checkpoint set for {}", n.getPath());
@@ -97,6 +99,8 @@ public class VersionServiceImpl extends AbstractService implements VersionServic
 
     /**
      * Explicitly creates a version for the nodes at each path provided.
+     * If the node doesn't have the versionable mixing, that mixin is
+     * added.
      * @param workspace the workspace in which the node resides
      * @param paths absolute paths to the nodes within the workspace
      * @throws RepositoryException
@@ -105,6 +109,10 @@ public class VersionServiceImpl extends AbstractService implements VersionServic
     public void createVersion(final Workspace workspace,
             final Collection<String> paths) throws RepositoryException {
         for (final String absPath : paths) {
+            final Node node = workspace.getSession().getNode(absPath);
+            if (!isVersioningEnabled(node)) {
+                enableVersioning(node);
+            }
             checkpoint(workspace, absPath);
         }
     }
@@ -169,6 +177,11 @@ public class VersionServiceImpl extends AbstractService implements VersionServic
 
     private static boolean isVersioningEnabled(final Node n) throws RepositoryException {
         return n.isNodeType(VERSIONABLE);
+    }
+
+    private void enableVersioning(final Node node) throws RepositoryException {
+        node.addMixin(VERSIONABLE);
+        node.getSession().save();
     }
 
     private static boolean isImplicitVersioningEnabled(final Node n) throws RepositoryException {
