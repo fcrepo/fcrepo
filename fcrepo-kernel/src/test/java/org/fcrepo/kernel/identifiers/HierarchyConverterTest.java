3b57136a35a45deaaa37d3a367718d5a1ce356e0
lsitu <lsitu@ucsd.edu>
2014-05-12 12:37:53 -0700

    Fix the HierarchyConverter to produce a consistent result
    - Implement to convert/revert the path for all path segments
    - Refactor the auto-hierarchy support for the content file
    
    Resolves: https://www.pivotaltracker.com/story/show/71140570

--- fcrepo-kernel/src/test/java/org/fcrepo/kernel/identifiers/HierarchyConverterTest.java
+++ fcrepo-kernel/src/test/java/org/fcrepo/kernel/identifiers/HierarchyConverterTest.java
@@ -17,6 +17,7 @@ package org.fcrepo.kernel.identifiers;
 
 import static com.google.common.base.Joiner.on;
 import static com.google.common.base.Strings.repeat;
+import static java.util.Arrays.asList;
 import static java.util.Collections.nCopies;
 import static java.util.regex.Pattern.compile;
 import static org.fcrepo.jcr.FedoraJcrTypes.FCR_CONTENT;
@@ -26,6 +27,7 @@ import static org.junit.Assert.assertTrue;
 import static org.modeshape.jcr.api.JcrConstants.JCR_CONTENT;
 import static org.slf4j.LoggerFactory.getLogger;
 
+import java.util.Iterator;
 import java.util.regex.Matcher;
 
 import org.junit.Before;
@@ -33,6 +35,10 @@ import org.junit.Test;
 import org.slf4j.Logger;
 
 public class HierarchyConverterTest {
+
+    private static final byte DEFAULT_LEVEL = 4;
+
+    private static final byte DEFAULT_LENGTH = 2;
 
     private static final String separator = "/";
 
@@ -44,7 +50,7 @@ public class HierarchyConverterTest {
 
     private static final String endingSegment = testIdSegments[2];
 
-    private final String testId = on(separator).join(testIdSegments);
+    private final String testId = "/" + on(separator).join(testIdSegments);
 
     private static final Logger log = getLogger(HierarchyConverterTest.class);
 
@@ -85,14 +91,45 @@ public class HierarchyConverterTest {
         testTranslator.setLevels(levels);
         testTranslator.setLength(length);
         final String result = testTranslator.reverse().convert(testId);
-        final String testRegexp =
-            startingSegments + separator + hierarchyRegexpSection(levels, length) + separator + endingSegment;
+        String testRegexp = "/";
+        for( final Iterator<String> it = asList(startingSegments.split(separator)).iterator(); it.hasNext();) {
+            testRegexp += hierarchyRegexpSection(levels, length) + separator + it.next()  + separator;
+        }
+        testRegexp += hierarchyRegexpSection(levels, length) + separator + endingSegment;
         final Matcher matches = compile(testRegexp).matcher(result);
         log.debug("Got result of translation: {}", result);
         log.debug("Matching against test pattern: {}", testRegexp);
         assertTrue("Did not find the appropriate modification to the input identifier!", matches.matches());
         final String shouldBeOriginal = testTranslator.convert(result);
         assertEquals("Didn't get original back!", testId, shouldBeOriginal);
+    }
+
+    @Test
+    public void sameIncomingPathOutput() {
+        testTranslator.setLevels(DEFAULT_LEVEL);
+        testTranslator.setLength(DEFAULT_LENGTH);
+        final String result = testTranslator.reverse().convert(testId);
+        final String shouldBeTheSame = testTranslator.reverse().convert(testId);
+        log.debug("Converted path {} to {} from incoming path {}!", result, shouldBeTheSame, testId);
+        assertEquals("Didn't get the same backend path!", result, shouldBeTheSame);
+    }
+
+    @Test
+    public void consistencyConversion() {
+        testTranslator.setLevels(DEFAULT_LEVEL);
+        testTranslator.setLength(DEFAULT_LENGTH);
+        for(int i = 0; i < 5; i++) {
+            final String result = testTranslator.reverse().convert(testId);
+            final String shouldBeTheSame = testTranslator.reverse().convert(testId);
+            log.debug("Didn't get the same backend path: expected {} but got {} from incoming path {}!", result, shouldBeTheSame, testId);
+            assertEquals("Didn't get the same backend path!", result, shouldBeTheSame);
+        }
+        final String hierarchyPath = testTranslator.reverse().convert(testId);
+        for(int i = 5; i > 0; i--) {
+            String reverted = testTranslator.convert(hierarchyPath);
+            log.debug("Reverted path {} to {} from original path {}!", hierarchyPath, reverted, testId);
+            assertEquals("Didn't get the same transparent path!", testId, reverted);
+        }
     }
 
     private static String hierarchyRegexpSection(final byte levels, final byte length) {
@@ -121,8 +158,19 @@ public class HierarchyConverterTest {
 
         result = testTranslator.reverse().convert(separator);
         log.debug("Separator identifier translated into {}.", separator, result);
-        assertEquals("Should have altered separator identifier to empty identifier!", "", testTranslator
+        assertEquals("Should not have altered separator identifier!", separator, testTranslator
                 .convert(result));
+    }
+
+    @Test
+    public void testOutgoingDoubleSlashPattern() {
+        testTranslator.setLevels(3);
+        testTranslator.setLength(1);
+        String hirarchy = testTranslator.reverse().convert(testId);
+        String outgoingPath = "a/b/c/" + hirarchy;
+        String result = testTranslator.convert(outgoingPath);
+        log.debug("Outgoing path {} converted to transparent path {}.", outgoingPath, result);
+        assertEquals("Should not have altered the outgoing double slash!", "/" + testId, result);
     }
 
     @Test
