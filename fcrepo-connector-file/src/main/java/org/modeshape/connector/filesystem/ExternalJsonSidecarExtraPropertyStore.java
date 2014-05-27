e05a959ce33422b5c3305585141cdbe0a8803079
Mike Durbin <md5wz@virginia.edu>
2014-05-27 14:01:52 -0400

    Add external federation properties store.
    Resolves: https://www.pivotaltracker.com/story/show/71234576

---
+++ fcrepo-connector-file/src/main/java/org/modeshape/connector/filesystem/ExternalJsonSidecarExtraPropertyStore.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2014 DuraSpace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.modeshape.connector.filesystem;
+
+import org.modeshape.jcr.cache.document.DocumentTranslator;
+
+import java.io.File;
+
+/**
+ * An extension of JsonSidecarExtraPropertyStore that stores the properties in a
+ * separate configured directory than the filesystem federation itself.  Because
+ * the class we're extending is package protected this class is in the
+ * org.modeshape.connector.filesystem package.
+ *
+ * @author Mike Durbin
+ */
+public class ExternalJsonSidecarExtraPropertyStore extends JsonSidecarExtraPropertyStore {
+
+    private FileSystemConnector connector;
+
+    private File propertyStoreRoot;
+
+    /**
+     * Default constructor.
+     * @param connector the FileSystemConnector for which this class will store properties.
+     * @param propertyStoreRoot the root of a filesystem into which properties will be
+     *                          serialized.
+     */
+    public ExternalJsonSidecarExtraPropertyStore(final FileSystemConnector connector,
+                                                 final DocumentTranslator translator,
+                                                 final File propertyStoreRoot) {
+        super(connector, translator);
+        this.connector = connector;
+        this.propertyStoreRoot = propertyStoreRoot;
+    }
+
+    @Override
+    protected File sidecarFile(final String id) {
+        final File propertyFileInFederation = super.sidecarFile(id);
+        final String relativePath
+                = propertyFileInFederation.getPath().substring(connector.fileFor("/").getPath().length());
+        final File file = new File(propertyStoreRoot,
+                connector.isRoot(id) ? "federation-root.modeshape.json" : relativePath);
+        if (!file.getParentFile().exists() && !file.getParentFile().mkdirs()) {
+            throw new RuntimeException("Unable to create directories " + file.getParentFile() + ".");
+        }
+        return file;
+    }
+}
---
+++ fcrepo-connector-file/src/test/java/org/modeshape/connector/filesystem/ExternalJsonSidecarExtraPropertyStoreTest.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2014 DuraSpace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.modeshape.connector.filesystem;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.modeshape.jcr.cache.document.DocumentTranslator;
+
+import java.io.File;
+import java.io.IOException;
+
+import static java.nio.file.Files.createTempDirectory;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Mike Durbin
+ */
+public class ExternalJsonSidecarExtraPropertyStoreTest {
+
+    private static final String FEDERATION_ROOT = "/federation-root";
+    private static final String FILE_PATH = "/federation-root/file";
+    @Test
+    public void testSidecarFile() throws IOException {
+        final File tmp = createTempDirectory("filesystem-federation").toFile();
+        try {
+            final FileSystemConnector mockConnector = mock(FileSystemConnector.class);
+            final DocumentTranslator mockTranslator = mock(DocumentTranslator.class);
+            when(mockConnector.fileFor("/")).thenReturn(new File(FEDERATION_ROOT));
+            when(mockConnector.isContentNode("/")).thenReturn(false);
+            when(mockConnector.fileFor("/file")).thenReturn(new File(FILE_PATH));
+            when(mockConnector.isContentNode("/file")).thenReturn(false);
+            when(mockConnector.fileFor("/file/fcr:content")).thenReturn(new File(FILE_PATH));
+            when(mockConnector.isContentNode("/file/fcr:content")).thenReturn(true);
+
+            final ExternalJsonSidecarExtraPropertyStore store
+                    = new ExternalJsonSidecarExtraPropertyStore(mockConnector, mockTranslator, tmp);
+            Assert.assertEquals(new File(tmp, ".modeshape.json"), store.sidecarFile("/"));
+            Assert.assertEquals(new File(tmp, "file.modeshape.json"), store.sidecarFile("/file"));
+            Assert.assertEquals(new File(tmp, "file.content.modeshape.json"), store.sidecarFile("/file/fcr:content"));
+        } finally {
+            FileUtils.deleteDirectory(tmp);
+        }
+    }
+
+}
