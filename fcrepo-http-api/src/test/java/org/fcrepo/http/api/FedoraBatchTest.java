4ec944ba586c6d5880df755278ac3c95856265c3
Chris Beer <cabeer@stanford.edu>
2014-09-24 14:12:15 -0700

    Cleaning up dependencies

--- fcrepo-http-api/src/test/java/org/fcrepo/http/api/FedoraBatchTest.java
+++ fcrepo-http-api/src/test/java/org/fcrepo/http/api/FedoraBatchTest.java
@@ -26,7 +26,6 @@ import static org.fcrepo.http.commons.test.util.PathSegmentImpl.createPathList;
 import static org.fcrepo.http.commons.test.util.TestHelpers.getUriInfoImpl;
 import static org.fcrepo.http.commons.test.util.TestHelpers.mockDatastream;
 import static org.fcrepo.http.commons.test.util.TestHelpers.mockSession;
-import static org.fcrepo.http.commons.test.util.TestHelpers.setField;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
@@ -38,6 +37,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.mock;
 import static org.mockito.MockitoAnnotations.initMocks;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
 
 import java.io.IOException;
 import java.io.InputStream;
