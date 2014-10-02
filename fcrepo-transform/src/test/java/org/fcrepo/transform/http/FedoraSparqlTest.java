02311a0c79282e391a1cb17deb5093078a65750e
lsitu <lsitu@ucsd.edu>
2014-10-02 10:29:39 -0700

    Add unit test for FedoraSparql.java class
    Resolves: https://www.pivotaltracker.com/story/show/78030948

--- fcrepo-transform/src/test/java/org/fcrepo/transform/http/FedoraSparqlTest.java
+++ fcrepo-transform/src/test/java/org/fcrepo/transform/http/FedoraSparqlTest.java
@@ -15,9 +15,28 @@
  */
 package org.fcrepo.transform.http;
 
-import org.fcrepo.http.commons.test.util.TestHelpers;
+import static javax.ws.rs.core.Response.Status.OK;
+import static javax.ws.rs.core.Response.ok;
+
+import static org.springframework.test.util.ReflectionTestUtils.setField;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFLanguages;
+import org.fcrepo.http.api.FedoraNodes;
+import org.fcrepo.http.commons.api.rdf.HttpIdentifierTranslator;
+import org.fcrepo.kernel.RdfLexicon;
+import org.fcrepo.kernel.rdf.IdentifierTranslator;
+import org.fcrepo.transform.http.responses.ResultSetStreamingOutput;
+import org.fcrepo.transform.sparql.JQLResultSet;
+import org.fcrepo.transform.sparql.SparqlServiceDescription;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.modeshape.jcr.api.NamespaceRegistry;
@@ -35,10 +54,19 @@ import javax.jcr.query.QueryManager;
 import javax.jcr.query.QueryResult;
 import javax.jcr.query.qom.Column;
 import javax.jcr.query.qom.QueryObjectModel;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
 import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Variant;
 
+import static org.apache.jena.riot.WebContent.contentTypeTextTSV;
+import static org.fcrepo.http.commons.domain.RDFMediaType.POSSIBLE_SPARQL_RDF_VARIANTS;
 import static org.fcrepo.http.commons.test.util.TestHelpers.getUriInfoImpl;
 import static org.fcrepo.http.commons.test.util.TestHelpers.mockSession;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
@@ -46,7 +74,7 @@ import static org.mockito.MockitoAnnotations.initMocks;
 
 /**
  * <p>FedoraSparqlTest class.</p>
- *
+ * @author lsitu
  * @author cbeer
  */
 public class FedoraSparqlTest {
@@ -54,6 +82,7 @@ public class FedoraSparqlTest {
     FedoraSparql testObj;
 
     private Session mockSession;
+
     private UriInfo uriInfo;
 
     @Mock
@@ -86,6 +115,17 @@ public class FedoraSparqlTest {
     @Mock
     private QueryResult mockResults;
 
+    @Mock
+    private IdentifierTranslator MockIdentifierTranslator;
+
+    @Mock
+    private Variant mockVariant;
+
+    @Mock
+    private Request mockRequest;
+
+    private static String testSparql = "SELECT ?s WHERE { ?x <" + RdfLexicon.DC_TITLE.getURI() + "> ?z }";
+
     @Before
     public void setUp() throws RepositoryException {
         initMocks(this);
@@ -93,10 +133,10 @@ public class FedoraSparqlTest {
         testObj = new FedoraSparql();
 
         this.uriInfo = getUriInfoImpl();
-        TestHelpers.setField(testObj, "uriInfo", uriInfo);
+        setField(testObj, "uriInfo", uriInfo);
 
         mockSession = mockSession(testObj);
-        TestHelpers.setField(testObj, "session", mockSession);
+        setField(testObj, "session", mockSession);
         when(mockSession.getWorkspace()).thenReturn(mockWorkspace);
 
         when(mockWorkspace.getQueryManager()).thenReturn(mockQueryManager);
@@ -114,14 +154,58 @@ public class FedoraSparqlTest {
         when(mockQueryFactory.createQuery(any(SelectQuery.class))).thenReturn(mockQuery);
 
         when(mockQuery.execute()).thenReturn(mockResults);
+
     }
 
     @Test
-    @Ignore
-    public void testRunSparqlQuery() {
-        //  final JQLResultSet resultSet = (JQLResultSet)testObj.runSparqlQuery(IOUtils.toInputStream("SELECT ?pid
-        // WHERE { ?pid <info:x> \"a\" }"), uriInfo);
+    public void testSparqlServiceDescription() {
+        final Response response = testObj.sparqlServiceDescription(mockRequest, uriInfo);
+        assertTrue(response.getStatus() == OK.getStatusCode());
+        final StreamingOutput stream = new StreamingOutput() {
+            @Override
+            public void write(final OutputStream output) {
+                final SparqlServiceDescription sd = new SparqlServiceDescription(mockSession, uriInfo);
+                final Lang rdfLang = RDFLanguages.contentTypeToLang(response.getMediaType().toString());
+                final Writer outWriter = new OutputStreamWriter(output);
+                sd.createServiceDescription().asModel().write(outWriter, rdfLang.getName());
+            }
+        };
+        final String expected = ok(stream).header("Content-Type",
+                RDFLanguages.contentTypeToLang(response.getMediaType().toString()))
+                .build().toString();
+        assertEquals(expected, response.toString());
+    }
 
-        //  assertEquals(mockResults, resultSet.getQueryResult());
+    @Test
+    public void testSparqlQueryForm() throws IOException, RepositoryException {
+        final String[] nsPrefix = {};
+        when(mockNamespaceRegistry.getPrefixes()).thenReturn(nsPrefix);
+        final Response response = testObj.sparqlQueryForm();
+        assertTrue(response.getStatus() == OK.getStatusCode());
+    }
+
+    @Test
+    public void testRunSparqlQuery() throws RepositoryException, IOException {
+        when(mockRequest.selectVariant(POSSIBLE_SPARQL_RDF_VARIANTS)).thenReturn(mockVariant);
+        when(mockVariant.getMediaType()).thenReturn(MediaType.valueOf(contentTypeTextTSV));
+        final InputStream input = new ByteArrayInputStream(testSparql.getBytes());
+        final Response response = testObj.runSparqlQuery(input, mockRequest, uriInfo);
+        assertTrue(response.getStatus() == OK.getStatusCode());
+        assertEquals(ok(new ResultSetStreamingOutput(new JQLResultSet(mockSession,
+                new HttpIdentifierTranslator(mockSession, FedoraNodes.class, uriInfo),
+                mockResults), mockVariant.getMediaType())).build().toString(),
+                response.toString());
+    }
+
+    @Test
+    public void testRunSparqlQueryWithHTMLForm() throws RepositoryException {
+        when(mockRequest.selectVariant(POSSIBLE_SPARQL_RDF_VARIANTS)).thenReturn(mockVariant);
+        when(mockVariant.getMediaType()).thenReturn(MediaType.valueOf(contentTypeTextTSV));
+        final Response response = testObj.runSparqlQuery(testSparql, mockRequest, uriInfo);
+        assertTrue(response.getStatus() == OK.getStatusCode());
+        assertEquals(ok(new ResultSetStreamingOutput(new JQLResultSet(mockSession,
+                new HttpIdentifierTranslator(mockSession, FedoraNodes.class, uriInfo),
+                mockResults), mockVariant.getMediaType())).build().toString(),
+                response.toString());
     }
 }
