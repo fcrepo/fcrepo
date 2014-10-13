c594619fb819e38c12eb612c5c0044803ea34f4f
Chris Beer <cabeer@stanford.edu>
2014-10-12 19:08:03 -0700

    Update setUpJMSBaseURIs to run every request

--- fcrepo-http-api/src/main/java/org/fcrepo/http/api/FedoraBaseResource.java
+++ fcrepo-http-api/src/main/java/org/fcrepo/http/api/FedoraBaseResource.java
@@ -87,28 +87,18 @@ abstract public class FedoraBaseResource extends AbstractResource {
     }
 
 
-
-    private static boolean baseURLSet = false;
     /**
      * Set the baseURL for JMS events.
      **/
     protected void setUpJMSBaseURIs(final UriInfo uriInfo) {
-        if ( !baseURLSet ) {
-            // set to true the first time this is run.  if there is an exception the first time, there
-            // will likely be an exception every time.  since this is run on each repository update,
-            // we should fail fast rather than retrying over and over.
-            baseURLSet = true;
-            try {
-                final URI baseURL = uriInfo.getBaseUri();
-                final Class<? extends FedoraBaseResource> klass = this.getClass();
-                LOGGER.debug(klass + ".init(): baseURL = " + baseURL.toString());
-                final ObservationManager obs = session().getWorkspace().getObservationManager();
-                final String json = "{\"baseURL\":\"" + baseURL.toString() + "\"}";
-                obs.setUserData(json);
-                LOGGER.trace(klass + ".init(): done");
-            } catch ( Exception ex ) {
-                LOGGER.warn("Error setting baseURL", ex);
-            }
+        try {
+            final URI baseURL = uriInfo.getBaseUri();
+            LOGGER.debug("setting baseURL = " + baseURL.toString());
+            final ObservationManager obs = session().getWorkspace().getObservationManager();
+            final String json = "{\"baseURL\":\"" + baseURL.toString() + "\"}";
+            obs.setUserData(json);
+        } catch ( Exception ex ) {
+            LOGGER.warn("Error setting baseURL", ex);
         }
     }
 
