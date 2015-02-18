d40b7188970aa291ae13e2d4f3fa09fadbf3d558
Yinlin Chen <ylchen@vt.edu>
2015-02-18 08:53:30 -0500

    Collapsible "if" statements should be merged.
    - merge collapsible "if" statements
    
    Resolves: https://jira.duraspace.org/browse/FCREPO-1333

--- fcrepo-kernel-impl/src/main/java/org/fcrepo/kernel/impl/FedoraResourceImpl.java
+++ fcrepo-kernel-impl/src/main/java/org/fcrepo/kernel/impl/FedoraResourceImpl.java
@@ -579,10 +579,8 @@ public class FedoraResourceImpl extends JcrTools implements FedoraJcrTypes, Fedo
              * a version of the subject node.
              */
                 final Property p = frozenNode.getProperty("jcr:frozenUuid");
-                if (p != null) {
-                    if (p.getString().equals(baseUUID)) {
-                        return frozenNode;
-                    }
+                if (p != null && p.getString().equals(baseUUID)) {
+                    return frozenNode;
                 }
             /*
              * Though a node with an id of the label was found, it wasn't the
