8c5d397795ada1960e1eb227d2dc55976e52f223
Aaron Coburn <acoburn@amherst.edu>
2015-06-01 18:45:15 -0400

    replace guava transform with java8 stream map

--- fcrepo-http-commons/src/main/java/org/fcrepo/http/commons/responses/RdfSerializationUtils.java
+++ fcrepo-http-commons/src/main/java/org/fcrepo/http/commons/responses/RdfSerializationUtils.java
@@ -15,8 +15,6 @@
  */
 package org.fcrepo.http.commons.responses;
 
-import static com.google.common.collect.ImmutableList.copyOf;
-import static com.google.common.collect.Iterables.transform;
 import static com.hp.hpl.jena.graph.NodeFactory.createURI;
 import static com.hp.hpl.jena.rdf.model.ResourceFactory.createProperty;
 import static com.hp.hpl.jena.rdf.model.ResourceFactory.createResource;
@@ -24,13 +22,11 @@ import static org.fcrepo.kernel.RdfLexicon.JCR_NAMESPACE;
 import static org.fcrepo.kernel.impl.rdf.JcrRdfTools.getRDFNamespaceForJcrNamespace;
 import static org.slf4j.LoggerFactory.getLogger;
 
+import java.util.function.Function;
 import java.util.Iterator;
 
-import com.google.common.base.Function;
-import com.google.common.collect.ImmutableList;
 import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.NodeIterator;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 import org.slf4j.Logger;
 
@@ -81,14 +77,11 @@ public final class RdfSerializationUtils {
      */
     public static String getFirstValueForPredicate(final Model rdf,
             final Node subject, final Node predicate) {
-        final NodeIterator statements = rdf.listObjectsOfProperty(createResource(subject.getURI()),
-                createProperty(predicate.getURI()));
-        // we'll take the first one we get
-        if (statements.hasNext()) {
-            return statements.next().asLiteral().getLexicalForm();
-        }
-        LOGGER.trace("No value found for predicate: {}", predicate);
-        return null;
+        return rdf.listObjectsOfProperty(createResource(subject.getURI()), createProperty(predicate.getURI()))
+                  .toList().stream()
+                  .map(x -> x.asLiteral().getLexicalForm())
+                  .findFirst()
+                  .orElse(null);
     }
 
     /**
@@ -102,12 +95,10 @@ public final class RdfSerializationUtils {
      */
     public static Iterator<String> getAllValuesForPredicate(final Model rdf,
             final Node subject, final Node predicate) {
-        final NodeIterator objects =
-            rdf.listObjectsOfProperty(createResource(subject.getURI()),
-                createProperty(predicate.getURI()));
-
-        final ImmutableList<RDFNode> copy = copyOf(objects);
-        return transform(copy, stringConverter).iterator();
+        return rdf.listObjectsOfProperty(createResource(subject.getURI()), createProperty(predicate.getURI()))
+                  .toList().stream()
+                  .map(x -> x.asLiteral().getLexicalForm())
+                  .iterator();
     }
 
 }
