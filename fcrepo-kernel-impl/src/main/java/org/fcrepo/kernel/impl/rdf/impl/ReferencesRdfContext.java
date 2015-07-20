beab8474900e5762612b94a8e3e23fc45687c9cd
Esmé Cowles <escowles@ucsd.edu>
2015-07-20 10:05:45 -0400

    Unwinding iteration logic into more functions/predicates

--- fcrepo-kernel-impl/src/main/java/org/fcrepo/kernel/impl/rdf/impl/ReferencesRdfContext.java
+++ fcrepo-kernel-impl/src/main/java/org/fcrepo/kernel/impl/rdf/impl/ReferencesRdfContext.java
@@ -17,11 +17,13 @@ package org.fcrepo.kernel.impl.rdf.impl;
 
 import static org.fcrepo.kernel.impl.identifiers.NodeResourceConverter.nodeConverter;
 import static org.fcrepo.kernel.impl.rdf.converters.ValueConverter.nodeForValue;
+import static java.util.Collections.addAll;
 import static javax.jcr.PropertyType.PATH;
 import static javax.jcr.PropertyType.REFERENCE;
 import static javax.jcr.PropertyType.WEAKREFERENCE;
 
 import com.google.common.base.Function;
+import com.google.common.base.Predicate;
 import com.google.common.collect.Iterators;
 import com.hp.hpl.jena.graph.Triple;
 import com.hp.hpl.jena.rdf.model.Resource;
@@ -29,12 +31,12 @@ import org.fcrepo.kernel.exception.RepositoryRuntimeException;
 import org.fcrepo.kernel.models.FedoraResource;
 import org.fcrepo.kernel.identifiers.IdentifierConverter;
 import org.fcrepo.kernel.impl.rdf.impl.mappings.PropertyToTriple;
+import javax.jcr.Node;
 import javax.jcr.Property;
 import javax.jcr.PropertyIterator;
 import javax.jcr.RepositoryException;
 import javax.jcr.Value;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -62,41 +64,53 @@ public class ReferencesRdfContext extends NodeRdfContext {
         throws RepositoryException {
         super(resource, idTranslator);
         property2triple = new PropertyToTriple(resource.getNode().getSession(), idTranslator);
-        putReferencesIntoContext(resource, property2triple);
-        putReferencesIntoContext(resource, property2proxyRefs);
+        putReferencesIntoContext(resource.getNode());
     }
 
-    private void putReferencesIntoContext(final FedoraResource resource, final Function f) throws RepositoryException {
-        concat(Iterators.concat(Iterators.transform(resource.getNode().getWeakReferences(), f)));
-        concat(Iterators.concat(Iterators.transform(resource.getNode().getReferences(), f)));
+    private void putReferencesIntoContext(final Node node) throws RepositoryException {
+        concat(Iterators.concat(Iterators.transform(
+            Iterators.concat(node.getReferences(), node.getWeakReferences()), property2triple)));
+        concat(Iterators.concat(Iterators.transform(Iterators.concat(Iterators.transform(
+            Iterators.concat(node.getReferences(), node.getWeakReferences()), potentialProxies)), triplesForValue)));
     }
 
-    private Function<Property, Iterator<Triple>> property2proxyRefs = new Function<Property, Iterator<Triple>>() {
+    /* References from LDP indirect containers are generated dynamically by LdpContainerRdfContext, so they won't
+       show up in getReferences()/getWeakReferences().  Instead, we should check referrers to see if they are
+       members of an IndirectContainer and generate the appropriate inbound references. */
+    private Function<Property, Iterator<Value>> potentialProxies = new Function<Property, Iterator<Value>>() {
         @Override
-        public Iterator<Triple> apply(final Property p) {
-            final Set<Iterator<Triple>> triples = new HashSet<>();
+        public Iterator<Value> apply(final Property p) {
+            final Set<Value> values = new HashSet<Value>();
             try {
                 for ( final PropertyIterator it = p.getParent().getProperties(); it.hasNext(); ) {
                     final Property potentialProxy = it.nextProperty();
                     if (potentialProxy.isMultiple()) {
-                        for ( Value v : potentialProxy.getValues() ) {
-                            triples.add(findProxies(v));
-                        }
+                        addAll(values, potentialProxy.getValues());
                     } else {
-                        triples.add(findProxies(potentialProxy.getValue()));
+                        values.add(potentialProxy.getValue());
                     }
                 }
             } catch (RepositoryException ex) {
                 throw new RepositoryRuntimeException(ex);
             }
-            return Iterators.concat(triples.iterator());
+            return Iterators.filter(values.iterator(), isReference);
         }
     };
-    private Iterator<Triple> findProxies(final Value v) throws RepositoryException {
-        if (v.getType() == PATH || v.getType() == REFERENCE || v.getType() == WEAKREFERENCE) {
-            return new LdpContainerRdfContext(nodeConverter.convert(nodeForValue(session(), v)), translator());
-        } else {
-            return Collections.<Triple>emptyIterator();
+    private Function<Value, Iterator<Triple>> triplesForValue = new Function<Value, Iterator<Triple>>() {
+        @Override
+        public Iterator<Triple> apply(final Value v) {
+            try {
+                return new LdpContainerRdfContext(nodeConverter.convert(nodeForValue(session(), v)), translator());
+            } catch (RepositoryException ex) {
+                throw new RepositoryRuntimeException(ex);
+            }
         }
-    }
+    };
+    private Predicate<Value> isReference = new Predicate<Value>() {
+        @Override
+        public boolean apply(final Value v) {
+            return v.getType() == PATH || v.getType() == REFERENCE || v.getType() == WEAKREFERENCE;
+        }
+    };
+
 }
