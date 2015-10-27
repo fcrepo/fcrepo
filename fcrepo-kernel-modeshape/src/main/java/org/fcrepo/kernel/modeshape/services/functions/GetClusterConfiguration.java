aaa7cf562d67ef878c3bed7ca825808665044f2c
Andrew Woods <awoods@duraspace.org>
2015-10-26 23:03:39 -0400

    Remove unused ISPN configuration file and clustering properties
    Resolves: https://jira.duraspace.org/browse/FCREPO-1739

--- fcrepo-kernel-modeshape/src/main/java/org/fcrepo/kernel/modeshape/services/functions/GetClusterConfiguration.java
+++ fcrepo-kernel-modeshape/src/main/java/org/fcrepo/kernel/modeshape/services/functions/GetClusterConfiguration.java
@@ -41,13 +41,10 @@ public class GetClusterConfiguration implements Function<Repository, Map<String,
     private static final Logger LOGGER = getLogger(GetClusterConfiguration.class);
 
     public static final String CLUSTER_NAME = "clusterName";
-    public static final String CACHE_MODE = "clusterCacheMode";
     public static final String NODE_ADDRESS = "clusterNodeAddress";
     public static final String PHYSICAL_ADDRESS = "clusterPhysicalAddress";
-    public static final String NODE_VIEW = "clusterNodeView";
     public static final String CLUSTER_SIZE = "clusterSize";
     public static final String CLUSTER_MEMBERS = "clusterMembers";
-    public static final int UNKNOWN_NODE_VIEW = -1;
 
     private final GetCacheManager getCacheManager = new GetCacheManager();
 
@@ -72,26 +69,16 @@ public class GetClusterConfiguration implements Function<Repository, Map<String,
             LOGGER.debug("Could not extract BinaryStore configuration", ex);
         }
 
-        final DefaultCacheManager cm = (DefaultCacheManager)getCacheManager.apply(input);
+        final DefaultCacheManager cm = getCacheManager.apply(input);
 
         if (cm == null) {
             LOGGER.debug("Could not access infinispan configuration");
             return result;
         }
 
-        final int nodeView;
-
-        if (cm.getTransport() != null) {
-            nodeView = cm.getTransport().getViewId() + 1;
-        } else {
-            nodeView = UNKNOWN_NODE_VIEW;
-        }
-
         result.put(CLUSTER_NAME, cm.getClusterName());
-        result.put(CACHE_MODE, cm.getCache().getCacheConfiguration().clustering().cacheMode().toString());
         result.put(NODE_ADDRESS, cm.getNodeAddress());
         result.put(PHYSICAL_ADDRESS, cm.getPhysicalAddresses());
-        result.put(NODE_VIEW, nodeView == UNKNOWN_NODE_VIEW ? "Unknown" : String.valueOf(nodeView));
         result.put(CLUSTER_SIZE, String.valueOf(cm.getClusterSize()));
         result.put(CLUSTER_MEMBERS, cm.getClusterMembers());
         return result;
