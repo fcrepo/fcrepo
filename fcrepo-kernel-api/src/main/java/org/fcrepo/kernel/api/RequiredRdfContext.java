322706a46bb2f09913b47976f525ea339624d464
ajs6f <ajs6f@virginia.edu>
2016-02-24 10:53:16 -0500

    Adding JavaDoc

--- fcrepo-kernel-api/src/main/java/org/fcrepo/kernel/api/RequiredRdfContext.java
+++ fcrepo-kernel-api/src/main/java/org/fcrepo/kernel/api/RequiredRdfContext.java
@@ -16,7 +16,8 @@
 package org.fcrepo.kernel.api;
 
 /**
- * A collection of RDF contexts that can be used to extract triples from FedoraResources
+ * A collection of RDF contexts that can be used to extract triples from FedoraResources. All implementations of the
+ * Fedora kernel are required to support these {@link TripleCategory}s, but may choose to support others.
  *
  * @author acoburn
  * @since Dec 4, 2015
