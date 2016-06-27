85106fedb310f9ba16f93556401d73dc2a010c7e
Aaron Coburn <acoburn@amherst.edu>
2016-06-27 08:07:19 -0400

    code review

--- fcrepo-http-api/src/main/java/org/fcrepo/http/api/url/HttpApiResources.java
+++ fcrepo-http-api/src/main/java/org/fcrepo/http/api/url/HttpApiResources.java
@@ -29,7 +29,6 @@ import com.hp.hpl.jena.rdf.model.Resource;
 import org.fcrepo.http.api.FedoraVersioning;
 import org.fcrepo.http.api.repository.FedoraRepositoryTransactions;
 import org.fcrepo.http.commons.api.rdf.UriAwareResourceModelFactory;
-import org.fcrepo.kernel.api.models.NonRdfSourceDescription;
 import org.fcrepo.kernel.api.models.FedoraBinary;
 import org.fcrepo.kernel.api.models.FedoraResource;
 import org.fcrepo.kernel.api.identifiers.IdentifierConverter;
@@ -62,16 +61,9 @@ public class HttpApiResources implements UriAwareResourceModelFactory {
             addNodeStatements(resource, uriInfo, model, s);
         }
 
-        if (resource instanceof NonRdfSourceDescription) {
-            final FedoraResource describedResource = resource.getDescribedResource();
-
-            if (describedResource instanceof FedoraBinary) {
-                addContentStatements(idTranslator, (FedoraBinary)describedResource, model);
-            }
-        } else if (resource instanceof FedoraBinary) {
-            addContentStatements(idTranslator, (FedoraBinary)resource, model);
+        if (resource.getDescribedResource() instanceof FedoraBinary) {
+            addContentStatements(idTranslator, (FedoraBinary)resource.getDescribedResource(), model);
         }
-
         return model;
     }
 
