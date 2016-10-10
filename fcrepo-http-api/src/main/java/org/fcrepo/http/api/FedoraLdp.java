ce045a2f8b5aeaa25bd85067feceb5bb1fccb152
Daniel Bernstein <dabylon@gmail.com>
2016-10-10 12:57:08 -0700

    Removes dangling else clause.

--- fcrepo-http-api/src/main/java/org/fcrepo/http/api/FedoraLdp.java
+++ fcrepo-http-api/src/main/java/org/fcrepo/http/api/FedoraLdp.java
@@ -566,7 +566,7 @@ public class FedoraLdp extends ContentExposingResource {
 
         if (throwable.getCause() != null) {
             checkForInsufficientStorageException(rootThrowable, throwable.getCause());
-        } else
+        }
 
         if (rootThrowable instanceof InvalidChecksumException) {
             throw (InvalidChecksumException) rootThrowable;
