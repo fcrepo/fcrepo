83784787202d8727f02c206cc39dd3bbbe53291b
dbernstein <dabylon@gmail.com>
2017-01-11 15:10:40 -0800

    Ensure Content-Type header for HEAD requests on containers mirrors wh… (#1155)
    * Ensure Content-Type header for HEAD requests on containers mirrors what we see a GET.
    
    Resolves https://jira.duraspace.org/browse/FCREPO-2159

--- fcrepo-http-api/src/test/java/org/fcrepo/integration/http/api/FedoraLdpIT.java
+++ fcrepo-http-api/src/test/java/org/fcrepo/integration/http/api/FedoraLdpIT.java
@@ -70,6 +70,7 @@ import static org.fcrepo.http.commons.domain.RDFMediaType.POSSIBLE_RDF_RESPONSE_
 import static org.fcrepo.http.commons.domain.RDFMediaType.POSSIBLE_RDF_VARIANTS;
 import static org.fcrepo.kernel.api.FedoraTypes.FCR_METADATA;
 import static org.fcrepo.kernel.api.RdfLexicon.BASIC_CONTAINER;
+import static org.fcrepo.kernel.api.RdfLexicon.CONTAINER;
 import static org.fcrepo.kernel.api.RdfLexicon.CONSTRAINED_BY;
 import static org.fcrepo.kernel.api.RdfLexicon.CONTAINS;
 import static org.fcrepo.kernel.api.RdfLexicon.CREATED_DATE;
@@ -149,6 +150,8 @@ import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.sparql.core.DatasetGraph;
 import org.apache.jena.sparql.core.Quad;
 import org.apache.jena.vocabulary.DC_11;
+
+import org.fcrepo.http.commons.domain.RDFMediaType;
 import org.fcrepo.http.commons.test.util.CloseableDataset;
 import org.glassfish.jersey.media.multipart.ContentDisposition;
 import org.junit.Ignore;
@@ -232,6 +235,46 @@ public class FedoraLdpIT extends AbstractResourceIT {
         try (final CloseableHttpResponse response = execute(headObjMethod)) {
             final Collection<String> links = getLinkHeaders(response);
             assertTrue("Didn't find LDP container link header!", links.contains(BASIC_CONTAINER_LINK_HEADER));
+        }
+    }
+
+    @Test
+    public void testHeadTurtleContentType() throws IOException {
+        testHeadDefaultContentType(RDFMediaType.TURTLE_WITH_CHARSET);
+    }
+
+    @Test
+    public void testHeadRDFContentType() throws IOException {
+        testHeadDefaultContentType(RDFMediaType.RDF_XML);
+    }
+
+    @Test
+    public void testHeadJSONLDContentType() throws IOException {
+        testHeadDefaultContentType(RDFMediaType.JSON_LD);
+    }
+
+    @Test
+    public void testHeadDefaultContentType() throws IOException {
+        testHeadDefaultContentType(null);
+    }
+
+    private void testHeadDefaultContentType(final String mimeType) throws IOException {
+        final String id = getRandomUniqueId();
+        createObjectAndClose(id);
+        addMixin(id, CONTAINER.getURI());
+
+        final HttpHead headObjMethod = headObjMethod(id);
+        String mt = mimeType;
+        if (mt != null) {
+            headObjMethod.addHeader("Accept", mt);
+        } else {
+            mt = RDFMediaType.TURTLE_WITH_CHARSET;
+        }
+        try (final CloseableHttpResponse response = execute(headObjMethod)) {
+            final Collection<String> contentTypes = getHeader(response, CONTENT_TYPE);
+            final String contentType = contentTypes.iterator().next();
+            assertTrue("Didn't find LDP valid content-type header: " + contentType +
+                    "; expected result: " + mt, contentType.contains(mt));
         }
     }
 
