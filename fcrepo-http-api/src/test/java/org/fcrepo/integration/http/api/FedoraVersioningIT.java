d7e5f18f8e79112a3a4d442bde32e3cc477f5091
bbpennel <bbpennel@email.unc.edu>
2018-03-06 12:01:07 -0500

    Added integration tests to verify creation of mementos with body.  Case with body but not datetime currently fails

--- fcrepo-http-api/src/test/java/org/fcrepo/integration/http/api/FedoraVersioningIT.java
+++ fcrepo-http-api/src/test/java/org/fcrepo/integration/http/api/FedoraVersioningIT.java
@@ -17,23 +17,26 @@
  */
 package org.fcrepo.integration.http.api;
 
+import static java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME;
 import static javax.ws.rs.core.HttpHeaders.CONTENT_TYPE;
 import static javax.ws.rs.core.HttpHeaders.LINK;
 import static javax.ws.rs.core.Response.Status.CREATED;
 import static javax.ws.rs.core.Response.Status.NO_CONTENT;
 import static javax.ws.rs.core.Response.Status.OK;
 import static org.apache.jena.graph.Node.ANY;
+import static org.apache.jena.graph.NodeFactory.createLiteral;
 import static org.apache.jena.graph.NodeFactory.createURI;
 import static org.fcrepo.http.api.FedoraVersioning.MEMENTO_DATETIME_HEADER;
 import static org.fcrepo.http.commons.domain.RDFMediaType.APPLICATION_LINK_FORMAT;
 import static org.fcrepo.kernel.api.FedoraTypes.FCR_VERSIONS;
+import static org.fcrepo.kernel.api.FedoraTypes.FEDORA_MEMENTO_DATETIME;
 import static org.fcrepo.kernel.api.RdfLexicon.VERSIONED_RESOURCE;
 import static org.fcrepo.kernel.api.RdfLexicon.VERSIONING_TIMEMAP_TYPE;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -41,6 +44,7 @@ import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import javax.ws.rs.core.Link;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -134,17 +138,107 @@ public class FedoraVersioningIT extends AbstractResourceIT {
         final String mementoDateTime =
                 RFC_1123_DATE_TIME.format(LocalDateTime.of(2000, 1, 1, 00, 00).atZone(ZoneOffset.UTC));
 
-        final HttpPost postMethod = postObjMethod(id + "/" + FCR_VERSIONS);
-        postMethod.addHeader(MEMENTO_DATETIME_HEADER, mementoDateTime);
-        try (final CloseableHttpResponse response = execute(postMethod)) {
-            assertEquals("Didn't get a CREATED response!", CREATED.getStatusCode(), getStatus(response));
+        final String mementoUri = createMemento(subjectUri, mementoDateTime, null);
+
+        final HttpGet httpGet = new HttpGet(mementoUri);
+        try (final CloseableHttpResponse response = execute(httpGet)) {
             assertMementoDatetimeHeaderMatches(response, mementoDateTime);
+
+            final CloseableDataset dataset = getDataset(response);
+            final DatasetGraph results = dataset.asDatasetGraph();
+            final Node subject = createURI(mementoUri);
+            final Node property = createURI(FEDORA_MEMENTO_DATETIME);
+            assertTrue("Did not find correct subject", results.contains(ANY, subject, ANY, ANY));
+            assertFalse("Memento response must not include memento datetime",
+                    results.contains(ANY, subject, property, ANY));
+        }
+    }
+
+    /**
+     * POST to create LDPCv without memento-datetime must ignore body
+     */
+    @Test
+    public void testCreateVersionWithBody() throws Exception {
+        createVersionedContainer(id, subjectUri);
+
+        final String mementoUri = createMementoWithBody(subjectUri, null);
+
+        final HttpGet httpGet = new HttpGet(mementoUri);
+        try (final CloseableDataset dataset = getDataset(httpGet)) {
+            final DatasetGraph results = dataset.asDatasetGraph();
+
+            final Node subject = createURI(subjectUri);
+            final Node property = createURI("info:test#label");
+
+            assertTrue("Memento created without must retain original state",
+                    results.contains(ANY, subject, property, createLiteral("foo")));
+            assertFalse("Memento created without datetime must ignore updates",
+                    results.contains(ANY, subject, property, createLiteral("bar")));
         }
     }
 
     @Test
     public void testCreateVersionWithDatetimeAndBody() throws Exception {
+        createVersionedContainer(id, subjectUri);
 
+        final String mementoDateTime =
+                RFC_1123_DATE_TIME.format(LocalDateTime.of(2000, 1, 1, 00, 00).atZone(ZoneOffset.UTC));
+
+        // Verify that the memento was created with the updated property
+        final String mementoUri = createMementoWithBody(subjectUri, mementoDateTime);
+        final Node mementoSubject = createURI(mementoUri);
+        final Node subject = createURI(subjectUri);
+        final Node property = createURI("info:test#label");
+
+        // Verify that the memento has the new property added to it
+        try (final CloseableHttpResponse response = execute(new HttpGet(mementoUri))) {
+            // Verify datetime was set correctly
+            assertMementoDatetimeHeaderMatches(response, mementoDateTime);
+
+            final CloseableDataset dataset = getDataset(response);
+            final DatasetGraph results = dataset.asDatasetGraph();
+
+            assertTrue("Memento must have original property",
+                    results.contains(ANY, mementoSubject, property, createLiteral("foo")));
+            assertTrue("Memento must have updated property",
+                    results.contains(ANY, mementoSubject, property, createLiteral("bar")));
+        }
+
+        // Verify that the original is unchanged
+        try (final CloseableDataset dataset = getDataset(new HttpGet(subjectUri))) {
+            final DatasetGraph results = dataset.asDatasetGraph();
+
+            assertTrue("Original must have original property",
+                    results.contains(ANY, subject, property, createLiteral("foo")));
+            assertFalse("Original must not have updated property",
+                    results.contains(ANY, subject, property, createLiteral("bar")));
+        }
+    }
+
+    private String createMemento(final String subjectUri, final String mementoDateTime, final String body)
+            throws Exception {
+        final HttpPost createVersionMethod = postObjMethod(id + "/" + FCR_VERSIONS);
+        createVersionMethod.addHeader(CONTENT_TYPE, "text/n3");
+        if (body != null) {
+            createVersionMethod.setEntity(new StringEntity("<> <info:test#label> \"bar\""));
+        }
+        if (mementoDateTime != null) {
+            createVersionMethod.addHeader(MEMENTO_DATETIME_HEADER, mementoDateTime);
+        }
+
+        // Create new memento of resource with changed triple
+        try (final CloseableHttpResponse response = execute(createVersionMethod)) {
+            assertEquals("Didn't get a CREATED response!", CREATED.getStatusCode(), getStatus(response));
+            assertMementoDatetimeHeaderPresent(response);
+
+            return EntityUtils.toString(response.getEntity());
+        }
+    }
+
+    private String createMementoWithBody(final String subjectUri, final String mementoDateTime) throws Exception {
+        // final String body = "<" + subjectUri + "> <info:test#label> \"bar\"";
+        final String body = "<> <info:test#label> \"bar\"";
+        return createMemento(subjectUri, mementoDateTime, body);
     }
 
     private void createVersionedContainer(final String id, final String subjectUri) throws Exception {
@@ -159,10 +253,14 @@ public class FedoraVersioningIT extends AbstractResourceIT {
         }
     }
 
-    private static void assertMementoDatetimeHeaderMatches(final CloseableHttpResponse response,
-            final String expected) {
+    private static void assertMementoDatetimeHeaderPresent(final CloseableHttpResponse response) {
         assertNotNull("No memento datetime header set in response",
                 response.getHeaders(MEMENTO_DATETIME_HEADER));
+    }
+
+    private static void assertMementoDatetimeHeaderMatches(final CloseableHttpResponse response,
+            final String expected) {
+        assertMementoDatetimeHeaderPresent(response);
         assertEquals("Response memento datetime did not match expected value",
                 expected, response.getFirstHeader(MEMENTO_DATETIME_HEADER).getValue());
     }
