83bf0aa021d0f6b71a6616a9e81956c09be8276e
Peter Eichman <peichman@umd.edu>
2018-05-07 17:13:44 -0400

    Created ShiroAuthenticationProvider (#1352)
    Modeshape authentication provider that inspects the Shiro SecurityUtils to get the current user. Uses a ShiroSecurityContext that is basically a thin wrapper around the Shiro current user Subject.
    
    Fixes: https://jira.duraspace.org/browse/FCREPO-2783

---
+++ fcrepo-auth-common/src/main/java/org/fcrepo/auth/common/ShiroAuthenticationProvider.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to DuraSpace under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * DuraSpace licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fcrepo.auth.common;
+
+import java.util.Map;
+
+import javax.jcr.Credentials;
+
+import org.apache.shiro.SecurityUtils;
+import org.modeshape.jcr.ExecutionContext;
+import org.modeshape.jcr.security.AuthenticationProvider;
+
+/**
+ * Modeshape authentication provider that gets its security context from the current Shiro Subject.
+ *
+ * @author peichman
+ */
+public class ShiroAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public ExecutionContext authenticate(final Credentials credentials, final String repositoryName,
+            final String workspaceName, final ExecutionContext repositoryContext,
+            final Map<String, Object> sessionAttributes) {
+
+        return repositoryContext.with(new ShiroSecurityContext(SecurityUtils.getSubject()));
+    }
+
+}
---
+++ fcrepo-auth-common/src/main/java/org/fcrepo/auth/common/ShiroSecurityContext.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to DuraSpace under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * DuraSpace licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fcrepo.auth.common;
+
+import org.apache.http.auth.BasicUserPrincipal;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
+import org.modeshape.jcr.security.SecurityContext;
+
+/**
+ * Security context that is simply a thin wrapper around a Shiro Subject.
+ * 
+ * @author peichman
+ */
+public class ShiroSecurityContext implements SecurityContext {
+
+    private Subject user;
+
+    private String userName;
+
+    /**
+     * Create a new security context using the given Shiro subject. That subject will typically be the value returned
+     * by a call to {@code SecurityUtils.getSubject()}.
+     *
+     * @param user subject to create the security context for
+     */
+    public ShiroSecurityContext(final Subject user) {
+        this.user = user;
+        final PrincipalCollection principals = user.getPrincipals();
+        final BasicUserPrincipal userPrincipal = principals.oneByType(BasicUserPrincipal.class);
+        if (userPrincipal != null) {
+            this.userName = userPrincipal.getName();
+        } else {
+            this.userName = null;
+        }
+    }
+
+    @Override
+    public boolean isAnonymous() {
+        return !user.isAuthenticated();
+    }
+
+    @Override
+    public String getUserName() {
+        return userName;
+    }
+
+    @Override
+    public boolean hasRole(final String roleName) {
+        // Under this custom PEP regime, all users have modeshape read and write
+        // roles.
+        if ("read".equals(roleName)) {
+            return true;
+        } else if ("write".equals(roleName)) {
+            return true;
+        } else if ("admin".equals(roleName)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void logout() {
+        // this method intentionally does nothing
+    }
+
+}
