b23fa7bc106c4d6e9025a2a2a0a1a62206637973
Yinlin Chen <ylchen@vt.edu>
2018-05-30 16:23:12 -0400

    Allow single-document ACLs using hash URIs for authorizations. (#1341)
    Enable a PATCH method to accept ACLs document
    
    Resolves: https://jira.duraspace.org/browse/FCREPO-2795

--- fcrepo-http-api/src/test/java/org/fcrepo/integration/http/api/FedoraAclIT.java
+++ fcrepo-http-api/src/test/java/org/fcrepo/integration/http/api/FedoraAclIT.java
@@ -17,13 +17,17 @@
  */
 package org.fcrepo.integration.http.api;
 
+import static javax.ws.rs.core.HttpHeaders.CONTENT_TYPE;
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 import static javax.ws.rs.core.Response.Status.CREATED;
+import static javax.ws.rs.core.Response.Status.NO_CONTENT;
 import static org.fcrepo.kernel.api.FedoraTypes.FCR_ACL;
 import static org.junit.Assert.assertEquals;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPatch;
 import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -84,4 +88,38 @@ public class FedoraAclIT extends AbstractResourceIT {
             assertEquals(subjectUri + "/x/" + FCR_ACL, aclLocation);
         }
     }
+
+    @Test
+    public void testPatchAcl() throws Exception {
+        createObjectAndClose(id);
+
+        final HttpPatch patch = new HttpPatch(subjectUri + "/" + FCR_ACL);
+        patch.addHeader(CONTENT_TYPE, "application/sparql-update");
+        patch.setEntity(new StringEntity("PREFIX acl: <http://www.w3.org/ns/auth/acl#> " +
+                "INSERT DATA { <#readAccess> acl:mode acl:write . }"));
+        assertEquals(NO_CONTENT.getStatusCode(), getStatus(patch));
+        // TODO add a test to verify inserted triples were actually on the resource.
+        // after the @GET method is implemented on FedoraAcl.java
+
+    }
+
+    @Test
+    public void testPatchAclOnAclResource() throws Exception {
+        createObjectAndClose(id);
+
+        final HttpPut put = new HttpPut(subjectUri + "/" + FCR_ACL);
+        final String aclLocation;
+        try (final CloseableHttpResponse response = execute(put)) {
+            assertEquals(CREATED.getStatusCode(), getStatus(response));
+            aclLocation = response.getFirstHeader("Location").getValue();
+        }
+
+        final HttpPatch patch = new HttpPatch(aclLocation + "/" + FCR_ACL);
+        patch.addHeader(CONTENT_TYPE, "application/sparql-update");
+        patch.setEntity(new StringEntity("PREFIX acl: <http://www.w3.org/ns/auth/acl#> " +
+                "INSERT DATA { <#readAccess> acl:mode acl:write . }"));
+        assertEquals(BAD_REQUEST.getStatusCode(), getStatus(patch));
+
+    }
+
 }
