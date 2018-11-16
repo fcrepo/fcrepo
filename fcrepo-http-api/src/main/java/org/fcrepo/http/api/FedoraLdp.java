acc465d584b39e32ba343d4a7c8fd1310fd77544
Andrew Woods <awoods@duraspace.org>
2018-11-16 13:59:37 -0500

    HEAD on memento to ignore Accept-Datetime heaader (#1478)
    Related to: https://jira.duraspace.org/browse/FCREPO-2932

--- fcrepo-http-api/src/main/java/org/fcrepo/http/api/FedoraLdp.java
+++ fcrepo-http-api/src/main/java/org/fcrepo/http/api/FedoraLdp.java
@@ -186,7 +186,7 @@ public class FedoraLdp extends ContentExposingResource {
         checkMementoPath();
 
         final String datetimeHeader = headers.getHeaderString(ACCEPT_DATETIME);
-        if (!isBlank(datetimeHeader)) {
+        if (!isBlank(datetimeHeader) && resource().isOriginalResource()) {
             return getMemento(datetimeHeader, resource());
         }
 
