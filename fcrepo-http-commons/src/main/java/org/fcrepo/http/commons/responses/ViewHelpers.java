29afccea01ed8855447ff1852f45b050d408f5d6
Peter Eichman <peichman@umd.edu>
2018-11-16 10:48:17 -0500

    Removed AclRdfContext class.
    Since it was doing nothing other than adding the fedora:writable triple, removed it entirely. The ViewHelpers::isWritable method now always returns true as a stopgap until we have a more robust method of determining the access modes a user is allowed on a resource.
    
    Fixes: https://jira.duraspace.org/browse/FCREPO-2938

--- fcrepo-http-commons/src/main/java/org/fcrepo/http/commons/responses/ViewHelpers.java
+++ fcrepo-http-commons/src/main/java/org/fcrepo/http/commons/responses/ViewHelpers.java
@@ -200,7 +200,8 @@ public class ViewHelpers {
      * @return whether the subject is writable
      */
     public boolean isWritable(final Graph graph, final Node subject) {
-        return getValue(graph, subject, WRITABLE.asNode()).filter("true"::equals).isPresent();
+        // XXX: always return true until we can determine a better way to control the HTML UI
+        return true;
     }
 
     /**
