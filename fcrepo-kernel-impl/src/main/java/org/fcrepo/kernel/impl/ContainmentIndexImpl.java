8d9e64bc1046ac719d24d2be49921b82742290b9
Peter Eichman <peichman@umd.edu>
2019-11-14 10:37:36 -0500

    Checkstyle fix
    Remove unused import statements

--- fcrepo-kernel-impl/src/main/java/org/fcrepo/kernel/impl/ContainmentIndexImpl.java
+++ fcrepo-kernel-impl/src/main/java/org/fcrepo/kernel/impl/ContainmentIndexImpl.java
@@ -29,9 +29,6 @@ import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.stream.Stream;
 
 /**
