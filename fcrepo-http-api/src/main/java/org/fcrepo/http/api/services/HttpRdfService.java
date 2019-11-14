60d96056975ff839e1a2696335281dec3d5d7dfa
B. Seeger <bseeger@users.noreply.github.com>
2019-11-14 16:39:41 -0500

    Adds HttpRdfService to help translate uris to fedora ids in the rdf. (#1575)
    * Adds HttpRdfService to help translate uris to ids in the rdf.
    
    HttpRdfService will translate from resource URI to internal resource ID
    
    part of https://jira.duraspace.org/browse/FCREPO-3060

---
+++ fcrepo-http-api/src/main/java/org/fcrepo/http/api/services/HttpRdfService.java
@@ -0,0 +1,159 @@
+/*
+ * Licensed to DuraSpace under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * DuraSpace licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.fcrepo.http.api.services;
+
+import static org.apache.jena.rdf.model.ModelFactory.createDefaultModel;
+import static org.fcrepo.kernel.api.rdf.DefaultRdfStream.fromModel;
+import static org.apache.jena.riot.RDFLanguages.contentTypeToLang;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.core.MediaType;
+import org.apache.jena.atlas.RuntimeIOException;
+import org.apache.jena.rdf.model.impl.StatementImpl;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RiotException;
+import org.fcrepo.http.commons.api.rdf.HttpIdentifierConverter;
+import org.fcrepo.kernel.api.exception.MalformedRdfException;
+import org.fcrepo.kernel.api.exception.RepositoryRuntimeException;
+import org.fcrepo.kernel.api.RdfStream;
+import org.slf4j.Logger;
+
+/**
+ * A service that will translate the resourceURI to Fedora ID in the Rdf InputStream
+ *
+ * @author bseeger
+ * @author bbpennel
+ * @since 2019-11-07
+ */
+
+public class HttpRdfService {
+
+    private static final Logger log = getLogger(HttpRdfService.class);
+
+    @Inject
+    private HttpIdentifierConverter idTranslator;
+
+    /**
+     * Parse the request body to a Model, with the URI to Fedora ID translations done.
+     *
+     * @param extResourceId the external ID of the Fedora resource
+     * @param stream the input stream containing the RDF
+     * @param contentType the media type of the RDF
+     * @return RdfStream containing triples from request body, with Fedora IDs in them
+     * @throws MalformedRdfException in case rdf json cannot be parsed
+     * @throws BadRequestException in the case where the RDF syntax is bad
+     */
+    public Model bodyToInternalModel(final String extResourceId, final InputStream stream,
+                                     final MediaType contentType)
+                                     throws RepositoryRuntimeException, BadRequestException {
+
+        final Model model = parseBodyAsModel(stream, contentType, extResourceId);
+        final List<Statement> insertStatements = new ArrayList<>();
+        final StmtIterator stmtIterator = model.listStatements();
+
+        while (stmtIterator.hasNext()) {
+            final Statement stmt = stmtIterator.nextStatement();
+            final String originalSubj = stmt.getSubject().getURI();
+            final String subj = idTranslator.inExternalDomain(originalSubj) ?
+                idTranslator.toInternalId(originalSubj) : originalSubj;
+
+            RDFNode obj = stmt.getObject();
+            if (stmt.getObject().isResource()) {
+                final String objString = stmt.getObject().asResource().getURI();
+                if (idTranslator.inExternalDomain(objString)) {
+                    obj = model.getResource(idTranslator.toInternalId(objString));
+                }
+            }
+
+            if (!subj.equals(originalSubj) || !obj.equals(stmt.getObject())) {
+                insertStatements.add(new StatementImpl(model.getResource(subj),
+                    stmt.getPredicate(), obj));
+
+                stmtIterator.remove();
+            }
+        }
+
+        model.add(insertStatements);
+
+        log.debug("Model: {}", model);
+        return model;
+    }
+
+     /**
+     * Parse the request body to a RdfStream, with the URI to Fedora ID translations done.
+     *
+     * @param extResourceId the external ID of the Fedora resource
+     * @param stream the input stream containing the RDF
+     * @param contentType the media type of the RDF
+     * @return RdfStream containing triples from request body, with Fedora IDs in them
+     * @throws MalformedRdfException in case rdf json cannot be parsed
+     * @throws BadRequestException in the case where the RDF syntax is bad
+     */
+    public RdfStream bodyToInternalStream(final String extResourceId, final InputStream stream,
+                                          final MediaType contentType)
+                                          throws RepositoryRuntimeException, BadRequestException {
+        final Model model = bodyToInternalModel(extResourceId, stream, contentType);
+
+        return fromModel(model.getResource(idTranslator.toInternalId(extResourceId)).asNode(), model);
+    }
+
+    /**
+     * Parse the request body as a Model.
+     *
+     * @param requestBodyStream rdf request body
+     * @param contentType content type of body
+     * @param extResourceId the external ID of the Fedora resource
+     * @return Model containing triples from request body
+     * @throws MalformedRdfException in case rdf json cannot be parsed
+     * @throws BadRequestException in the case where the RDF syntax is bad
+     */
+    public static Model parseBodyAsModel(final InputStream requestBodyStream,
+                                         final MediaType contentType,
+                                         final String extResourceId) throws BadRequestException,
+                                         RepositoryRuntimeException {
+
+        final Lang format = contentTypeToLang(contentType.toString());
+
+        final Model inputModel;
+        try {
+            inputModel = createDefaultModel();
+            inputModel.read(requestBodyStream, extResourceId, format.getName().toUpperCase());
+            return inputModel;
+        } catch (final RiotException e) {
+            throw new BadRequestException("RDF was not parsable: " + e.getMessage(), e);
+
+        } catch (final RuntimeIOException e) {
+            if (e.getCause() instanceof JsonParseException) {
+                throw new MalformedRdfException(e.getCause());
+            }
+            throw new RepositoryRuntimeException(e);
+        }
+    }
+
+}
---
+++ fcrepo-http-api/src/test/java/org/fcrepo/http/api/services/HttpRdfServiceTest.java
@@ -0,0 +1,129 @@
+/*
+ * Licensed to DuraSpace under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * DuraSpace licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fcrepo.http.api.services;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.when;
+import static org.fcrepo.kernel.api.RdfCollectors.toModel;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.apache.jena.vocabulary.DC;
+import org.apache.jena.vocabulary.DCTerms;
+import javax.ws.rs.core.MediaType;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
+import org.junit.runner.RunWith;
+import org.fcrepo.http.commons.api.rdf.HttpIdentifierConverter;
+import org.fcrepo.kernel.api.models.FedoraResource;
+import org.fcrepo.kernel.api.RdfStream;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.slf4j.Logger;
+
+/**
+ * Unit tests for HttpRdfService
+ * @author bseeger
+ * @since 2019-11-08
+ */
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class HttpRdfServiceTest {
+
+    private static final Logger log = getLogger(HttpRdfService.class);
+
+    @Mock
+    HttpIdentifierConverter idTranslator;
+
+    @Mock
+    FedoraResource resource;
+
+    @InjectMocks
+    private HttpRdfService httpRdfService;
+
+    private static final String FEDORA_URI_1 = "http://www.example.com/fedora/rest/resource1";
+    private static final String FEDORA_URI_2 = "http://www.example.com/fedora/rest/resource2";
+    private static final String FEDORA_ID_1 = "info:fedora/resource1";
+    private static final String FEDORA_ID_2 = "info:fedora/resource2";
+    private static final String NON_FEDORA_URI = "http://www.otherdomain.org/resource5";
+    private static final String RDF =
+                "@prefix dc: <"  + DC.getURI() + "> ." +
+                "@prefix dcterms: <"  + DCTerms.getURI() + "> ." +
+                "<" + FEDORA_URI_1 + "> dc:title 'fancy title' ;" +
+                "    dcterms:isPartOf <" + NON_FEDORA_URI + "> ;" +
+                "    dcterms:isPartOf <" + FEDORA_URI_2 + "> .";
+
+    private static final MediaType CONTENT_TYPE = new MediaType("text", "turtle");
+
+    @Before
+    public void setup(){
+        when(idTranslator.toInternalId(FEDORA_URI_1)).thenReturn(FEDORA_ID_1);
+        when(idTranslator.toInternalId(FEDORA_URI_2)).thenReturn(FEDORA_ID_2);
+        when(idTranslator.inExternalDomain(FEDORA_URI_1)).thenReturn(true);
+        when(idTranslator.inExternalDomain(FEDORA_URI_2)).thenReturn(true);
+        when(idTranslator.inExternalDomain(NON_FEDORA_URI)).thenReturn(false);
+        when(resource.getId()).thenReturn(FEDORA_ID_1);
+
+        log.debug("Rdf is: {}", RDF);
+    }
+
+    @Test
+    public void testGetModelFromInputStream() {
+
+        final InputStream requestBodyStream = new ByteArrayInputStream(RDF.getBytes());
+        final Model model = httpRdfService.bodyToInternalModel(FEDORA_URI_1, requestBodyStream,
+            CONTENT_TYPE);
+
+        assertFalse(model.isEmpty());
+
+        verifyTriples(model);
+    }
+
+    @Test
+    public void testGetRdfStreamFromInputStream() {
+
+        final InputStream requestBodyStream = new ByteArrayInputStream(RDF.getBytes());
+        final RdfStream stream = httpRdfService.bodyToInternalStream(FEDORA_URI_1, requestBodyStream,
+            CONTENT_TYPE);
+
+        assertTrue(stream.toString().length() > 0);
+        verifyTriples(stream);
+    }
+
+    private void verifyTriples(final Model model)  {
+
+        final Resource fedoraResource = model.createResource(FEDORA_ID_1);
+
+        // make sure it changed the fedora uri to id, but didn't touch the non fedora domain
+        assertTrue(model.contains(fedoraResource, DCTerms.isPartOf, model.createResource(NON_FEDORA_URI)));
+
+        // make sure it translated the fedora uri to fedora id for this triple in both the subject and object
+        assertTrue(model.contains(fedoraResource, DCTerms.isPartOf, model.createResource(FEDORA_ID_2)));
+
+        // make sure there are no triples that have the subject of the fedora uri
+        assertFalse(model.containsResource(model.createResource(FEDORA_URI_1)));
+    }
+
+    private void verifyTriples(final RdfStream rdfStream) {
+        verifyTriples(rdfStream.collect(toModel()));
+    }
+}
