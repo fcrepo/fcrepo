470fadbd1c149ef8e4bc77ff733933fd529f6a02
Jon Roby <jonathan.roby@umanitoba.ca>
2020-01-29 11:19:43 -0600

    FCREPO-3177 - Return 303 on GET for non-existant resource
    - Added unique PathNotFoundExceptionMapper and mock test for that file
    
    Resolves: https://jira.lyrasis.org/browse/FCREPO-3177

---
+++ fcrepo-http-commons/src/main/java/org/fcrepo/http/commons/exceptionhandlers/PathNotFoundExceptionMapper.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to DuraSpace under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * DuraSpace licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fcrepo.http.commons.exceptionhandlers;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.fcrepo.kernel.api.exception.PathNotFoundException;
+import org.slf4j.Logger;
+
+/**
+ * Catch PathNotFoundException
+ *
+ * @author robyj
+ */
+@Provider
+public class PathNotFoundExceptionMapper implements
+        ExceptionMapper<PathNotFoundException>, ExceptionDebugLogging {
+
+    Boolean showStackTrace = true;
+
+    private static final Logger LOGGER =
+        getLogger(PathNotFoundExceptionMapper.class);
+
+    @Override
+    public Response toResponse(final PathNotFoundException e) {
+
+        LOGGER.error("Exception intercepted by PathNotFoundExceptionMapper: {}\n", e.getMessage());
+        debugException(this, e, LOGGER);
+        return Response.status(Response.Status.NOT_FOUND).
+                entity("Error: " + e.getMessage()).build();
+    }
+
+    /**
+     * Set whether the full stack trace should be returned as part of the
+     * error response. This may be a bad idea if the stack trace is exposed
+     * to the public.
+     * @param showStackTrace the boolean value of showing stack trace
+     */
+    public void setShowStackTrace(final Boolean showStackTrace) {
+        this.showStackTrace = showStackTrace;
+    }
+}
+
---
+++ fcrepo-http-commons/src/test/java/org/fcrepo/http/commons/exceptionhandlers/PathNotFoundExceptionMapperTest.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed to DuraSpace under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * DuraSpace licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fcrepo.http.commons.exceptionhandlers;
+
+import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import javax.ws.rs.core.Response;
+
+import org.fcrepo.kernel.api.exception.PathNotFoundException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * <p>PathNotFoundExceptionMapperTest class.</p>
+ *
+ * @author robyj
+ */
+public class PathNotFoundExceptionMapperTest {
+
+    private PathNotFoundExceptionMapper testObj;
+
+    @Before
+    public void setUp() {
+        testObj = new PathNotFoundExceptionMapper();
+    }
+
+    @Test
+    public void testToResponse() {
+        final PathNotFoundException input = new PathNotFoundException("xyz");
+        Response actual = testObj.toResponse(input);
+        assertEquals(NOT_FOUND.getStatusCode(), actual.getStatus());
+        assertTrue(actual.getEntity() != "xyz");
+        testObj.showStackTrace = false;
+        actual = testObj.toResponse(input);
+        assertNotEquals(INTERNAL_SERVER_ERROR.getStatusCode(), actual.getStatus());
+        assertNotNull(actual.getEntity());
+    }
+}
