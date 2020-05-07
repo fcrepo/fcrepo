05a65ff4804046dc6801868ceeb69ad8fc6088a7
dbernstein <dabylon@gmail.com>
2020-05-07 10:47:50 -0700

    Ensures deployment to sonatype only happens on the linux build. (#1676)
    Resolves: https://jira.lyrasis.org/browse/FCREPO-3303

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,13 @@
-if [ ! -z "$TRAVIS_TAG" ]
+# only deploy on linux
+if  [ "$TRAVIS_OS_NAME" = "linux" ]
 then
-    echo "on a tag -> $TRAVIS_TAG and therefore we will do nothing. Tagged releases are deployed to sonatype manually."
+  if [ ! -z "$TRAVIS_TAG" ]
+  then
+      echo "on a tag -> $TRAVIS_TAG and therefore we will do nothing. Tagged releases are deployed to sonatype manually."
+  else
+      echo "not on a tag -> deploying to sonatype snapshot repo..."
+      ./mvnw clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
+  fi
 else
-    echo "not on a tag -> deploying to sonatype snapshot repo..."
-    ./mvnw clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
+   echo "Ignoring deploy step since we are not on linux. (TRAVIS_OS_NAME=${TRAVIS_OS_NAME})"
 fi
-
