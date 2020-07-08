532b76ac65af6c9e9e9dc895b25719459d87a04f
dbernstein <dabylon@gmail.com>
2020-07-08 10:34:56 -0700

    Removes unnecessary concatenation and moves method variables into constants.

--- fcrepo-search-impl/src/main/java/org/fcrepo/search/impl/DbSearchIndexImpl.java
+++ fcrepo-search-impl/src/main/java/org/fcrepo/search/impl/DbSearchIndexImpl.java
@@ -43,7 +43,6 @@ import org.springframework.transaction.support.TransactionTemplate;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.sql.DataSource;
-import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
@@ -73,6 +72,20 @@ public class DbSearchIndexImpl implements SearchIndex {
     private static final Logger LOGGER = LoggerFactory.getLogger(DbSearchIndexImpl.class);
     private static final String SIMPLE_SEARCH_TABLE = "simple_search";
     private static final String DDL = "sql/default-search-index.sql";
+    private static final String DELETE_FROM_INDEX_SQL = "DELETE FROM simple_search WHERE fedora_id = :fedora_id;";
+    private static final String UPDATE_INDEX_SQL =
+            "UPDATE simple_search SET modified = :modified, content_size = :content_size, mime_type =:mime_type " +
+                    "WHERE fedora_id = :fedora_id;";
+    private static final String INSERT_INTO_INDEX_SQL =
+            "INSERT INTO simple_search (fedora_id, modified, created, content_size, mime_type) VALUES" +
+            "(:fedora_id, :modified, :modified, :content_size, :mime_type)";
+    private static final String SELECT_BY_FEDORA_ID =
+            "SELECT fedora_id FROM simple_search WHERE fedora_id = :fedora_id";
+    private static final String FEDORA_ID_PARAM = "fedora_id";
+    private static final String MODIFIED_PARAM = "modified";
+    private static final String CONTENT_SIZE_PARAM = "content_size";
+    private static final String MIME_TYPE_PARAM = "mime_type";
+    private static final String CREATED_PARAM = "created";
 
     @Inject
     private DataSource dataSource;
@@ -126,9 +139,8 @@ public class DbSearchIndexImpl implements SearchIndex {
                 try {
                     final var instant = InstantParser.parse(object);
                     final var paramName = "param" + paramCount++;
-                    whereClauses.add(field + " " + operation.getStringValue() +
-                            " :" + paramName);
-                    parameterSource.addValue(paramName, new Date(instant.toEpochMilli()), Types.TIMESTAMP);
+                    whereClauses.add(field + " " + operation.getStringValue() + " :" + paramName);
+                    parameterSource.addValue(paramName, new Timestamp(instant.toEpochMilli()), Types.TIMESTAMP);
                 } catch (Exception ex) {
                     throw new InvalidQueryException(ex.getMessage());
                 }
@@ -189,38 +201,28 @@ public class DbSearchIndexImpl implements SearchIndex {
     public void addUpdateIndex(final ResourceHeaders resourceHeaders) {
         final var fullId = resourceHeaders.getId();
         final var selectParams = new MapSqlParameterSource();
-        final var fedoraIdParam = FEDORA_ID.toString();
-        selectParams.addValue(fedoraIdParam, fullId);
-        final var result = jdbcTemplate.queryForList("SELECT " + FEDORA_ID + " FROM " + SIMPLE_SEARCH_TABLE +
-                        " WHERE " + FEDORA_ID + " = :" + fedoraIdParam, selectParams);
+        selectParams.addValue(FEDORA_ID_PARAM, fullId);
+        final var result =
+                jdbcTemplate.queryForList(SELECT_BY_FEDORA_ID,
+                        selectParams);
 
         final var txId = UUID.randomUUID().toString();
         executeInDbTransaction(txId, status -> {
             try {
 
                 final var params = new MapSqlParameterSource();
-                params.addValue(fedoraIdParam, fullId);
-                final var modifiedParam = "modified";
-                final var contentSizeParam = "content_size";
-                final var mimetypeParam = "mime_type";
-                params.addValue(modifiedParam, new Timestamp(resourceHeaders.getLastModifiedDate().toEpochMilli()));
-                params.addValue(mimetypeParam, resourceHeaders.getMimeType());
-                params.addValue(contentSizeParam, resourceHeaders.getContentSize());
+                params.addValue(FEDORA_ID_PARAM, fullId);
+                params.addValue(MODIFIED_PARAM, new Timestamp(resourceHeaders.getLastModifiedDate().toEpochMilli()));
+                params.addValue(MIME_TYPE_PARAM, resourceHeaders.getMimeType());
+                params.addValue(CONTENT_SIZE_PARAM, resourceHeaders.getContentSize());
                 final String sql;
                 if (result.size() > 0) {
                     //update
-                    sql = "UPDATE " + SIMPLE_SEARCH_TABLE +
-                            " SET modified=:" + modifiedParam + ", content_size=:" + contentSizeParam + ", " +
-                            "mime_type=:" + mimetypeParam + " WHERE fedora_id = :" + fedoraIdParam + ";";
-                    jdbcTemplate.update(sql, params);
+                    jdbcTemplate.update(UPDATE_INDEX_SQL, params);
                 } else {
-                    final var createdParam = "created";
-                    params.addValue(createdParam, new Timestamp(resourceHeaders.getCreatedDate().toEpochMilli()));
-                    sql = "INSERT INTO " + SIMPLE_SEARCH_TABLE + " (fedora_id, modified, created, content_size, " +
-                            "mime_type) VALUES(:" + fedoraIdParam + ", :" + modifiedParam + ", :" + createdParam + ", :"
-                            + contentSizeParam + ", :" + mimetypeParam + ")";
+                    params.addValue(CREATED_PARAM, new Timestamp(resourceHeaders.getCreatedDate().toEpochMilli()));
+                    jdbcTemplate.update(INSERT_INTO_INDEX_SQL, params);
                 }
-                jdbcTemplate.update(sql, params);
                 return null;
             } catch (Exception e) {
                 status.setRollbackOnly();
@@ -233,9 +235,8 @@ public class DbSearchIndexImpl implements SearchIndex {
     public void removeFromIndex(final FedoraId fedoraId) {
         try {
             final var params = new MapSqlParameterSource();
-            params.addValue("fedora_id", fedoraId.getFullId());
-            final var sql = "DELETE FROM " + SIMPLE_SEARCH_TABLE + " WHERE fedora_id = :fedora_id;";
-            jdbcTemplate.update(sql, params);
+            params.addValue(FEDORA_ID_PARAM, fedoraId.getFullId());
+            jdbcTemplate.update(DELETE_FROM_INDEX_SQL, params);
         } catch (DataAccessException ex) {
             throw new RepositoryRuntimeException("Failed to delete search index entry for " + fedoraId.getFullId());
         }
