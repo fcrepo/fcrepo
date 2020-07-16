475e5f06c3c016483f20dd8f52666486428fddaf
dbernstein <dabylon@gmail.com>
2020-07-16 12:55:30 -0700

    Impoves log message in deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 if [ ! -z "$TRAVIS_TAG" ]
 then
-    echo "on a tag -> $TRAVIS_TAG and therefore we will do nothing. Tagged releases are deployed to sonatype manually."
+    echo "on a tag -> $TRAVIS_TAG and therefore we will do nothing. Tagged releases are deployed to sonatype and dockerhub manually."
 else
     echo "not on a tag -> deploying to dockerhub and sonatype..."
     FCREPO_VERSION=$(./mvnw org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)
