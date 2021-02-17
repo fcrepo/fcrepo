358b2b309039d22ffe0a5c6707508b21dd8497a1
Peter Winckles <peter.winckles@wisc.edu>
2021-02-17 10:31:45 -0600

    remove import

--- fcrepo-configs/src/main/java/org/fcrepo/config/ConditionOnProperty.java
+++ fcrepo-configs/src/main/java/org/fcrepo/config/ConditionOnProperty.java
@@ -22,7 +22,6 @@ import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.ConfigurationCondition;
 import org.springframework.core.type.AnnotatedTypeMetadata;
