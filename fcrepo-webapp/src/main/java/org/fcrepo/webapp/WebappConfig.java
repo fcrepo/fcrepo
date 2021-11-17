1583820e3fab85cdf89868ef8b1440661bd42b36
Michael Ritter <emericaskate15@gmail.com>
2021-11-17 15:08:11 -0700

    Enable async

--- fcrepo-webapp/src/main/java/org/fcrepo/webapp/WebappConfig.java
+++ fcrepo-webapp/src/main/java/org/fcrepo/webapp/WebappConfig.java
@@ -27,6 +27,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
@@ -41,6 +42,7 @@ import com.google.common.eventbus.EventBus;
  * @author pwinckles
  */
 @Configuration
+@EnableAsync
 @EnableScheduling
 public class WebappConfig {
 
