4a9ae5cca0b4435deea79ba919913c9495e05acf
Jared Whiklo <jwhiklo@gmail.com>
2022-01-12 13:06:43 -0600

    Move ACL cache clear to ReplacePropertiesService to catch both PATCH and PUT requests

--- fcrepo-kernel-impl/src/main/java/org/fcrepo/kernel/impl/services/UpdatePropertiesServiceImpl.java
+++ fcrepo-kernel-impl/src/main/java/org/fcrepo/kernel/impl/services/UpdatePropertiesServiceImpl.java
@@ -8,12 +8,9 @@ package org.fcrepo.kernel.impl.services;
 
 import static org.fcrepo.kernel.api.RdfCollectors.toModel;
 
-import java.util.Optional;
-
 import javax.inject.Inject;
 
 import org.fcrepo.kernel.api.Transaction;
-import org.fcrepo.kernel.api.auth.ACLHandle;
 import org.fcrepo.kernel.api.exception.AccessDeniedException;
 import org.fcrepo.kernel.api.exception.ItemNotFoundException;
 import org.fcrepo.kernel.api.exception.MalformedRdfException;
@@ -24,14 +21,12 @@ import org.fcrepo.kernel.api.services.UpdatePropertiesService;
 import org.fcrepo.persistence.api.PersistentStorageSessionManager;
 import org.fcrepo.persistence.api.exceptions.PersistentItemNotFoundException;
 import org.fcrepo.persistence.api.exceptions.PersistentStorageException;
-import org.springframework.stereotype.Component;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.update.UpdateAction;
 import org.apache.jena.update.UpdateFactory;
 import org.apache.jena.update.UpdateRequest;
-
-import com.github.benmanes.caffeine.cache.Cache;
+import org.springframework.stereotype.Component;
 
 /**
  * This class implements the update properties operation.
@@ -47,9 +42,6 @@ public class UpdatePropertiesServiceImpl extends AbstractService implements Upda
     @Inject
     private PersistentStorageSessionManager persistentStorageSessionManager;
 
-    @Inject
-    private Cache<String, Optional<ACLHandle>> authHandleCache;
-
     @Override
     public void updateProperties(final Transaction tx, final String userPrincipal,
                                  final FedoraId fedoraId, final String sparqlUpdateStatement)
@@ -61,10 +53,6 @@ public class UpdatePropertiesServiceImpl extends AbstractService implements Upda
             final UpdateRequest request = UpdateFactory.create(sparqlUpdateStatement, fedoraId.getFullId());
             UpdateAction.execute(request, model);
             replacePropertiesService.perform(tx, userPrincipal, fedoraId, model);
-            if (fedoraId.isAcl()) {
-                // Flush ACL cache on any ACL creation/update/deletion.
-                authHandleCache.invalidateAll();
-            }
         } catch (final PersistentItemNotFoundException ex) {
             throw new ItemNotFoundException(ex.getMessage(), ex);
         } catch (final PersistentStorageException ex) {
