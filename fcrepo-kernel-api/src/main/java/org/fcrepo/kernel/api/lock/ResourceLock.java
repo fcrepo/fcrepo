9a9d0927a18f3fc37091a67569670f6e8a2df73a
Peter Winckles <pwinckles@pm.me>
2023-01-14 08:01:53 -0600

    resource locking tweaks

--- fcrepo-kernel-api/src/main/java/org/fcrepo/kernel/api/lock/ResourceLock.java
+++ fcrepo-kernel-api/src/main/java/org/fcrepo/kernel/api/lock/ResourceLock.java
@@ -7,6 +7,10 @@ package org.fcrepo.kernel.api.lock;
 
 /**
  * A simple lock with a type, transaction and resource.
+ * <p>
+ * It is essential that implementations of this interface implement {@link Object#equals(Object)} and
+ * {@link Object#hashCode()} so that all locks with the same transaction id and resource id are considered equal.
+ *
  * @author whikloj
  * @since 6.3.1
  */
@@ -35,6 +39,15 @@ public interface ResourceLock {
      * @return true if matches.
      */
     boolean hasLockType(final ResourceLockType lockType);
+
+    /**
+     * If an exclusive lock is requested, returns true only if this lock is exclusive. If non-exclusive is requested,
+     * then true is always returned.
+     *
+     * @param lockType the type of lock requested
+     * @return true if this lock is adequate
+     */
+    boolean isAdequate(final ResourceLockType lockType);
 
     /**
      * @return the transaction ID for this lock.
