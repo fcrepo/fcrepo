9053dfcb4ba855d8488daa15e474f6f40dafbb3d
Michael Ritter <emericaskate15@gmail.com>
2023-05-08 13:32:23 -0600

    Add configuration for version info

---
+++ fcrepo-configs/src/main/java/org/fcrepo/config/SystemInfoConfig.java
@@ -0,0 +1,31 @@
+/*
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree.
+ */
+package org.fcrepo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Small config to get info from git properties and the implementation version
+ *
+ * @author mikejritter
+ */
+@Configuration
+public class SystemInfoConfig extends BasePropsConfig {
+
+    public static final String GIT_COMMIT = "git.commit.id.abbrev";
+
+    @Value("${" + GIT_COMMIT + ":#{null}}")
+    private String gitCommit;
+
+    public String getGitCommit() {
+        return gitCommit;
+    }
+
+    public String getImplementationVersion() {
+        return SystemInfoConfig.class.getPackage().getImplementationVersion();
+    }
+}
