18d2c0e70b7f02270658f238739b2087f4defd66
Michael Ritter <emericaskate15@gmail.com>
2023-06-08 13:03:22 -0600

    Updates to ConcurrentUpdateException: * Use two messages for log/http messages * Add Transaction ids for inclusion in error message

--- fcrepo-kernel-api/src/main/java/org/fcrepo/kernel/api/exception/ConcurrentUpdateException.java
+++ fcrepo-kernel-api/src/main/java/org/fcrepo/kernel/api/exception/ConcurrentUpdateException.java
@@ -5,6 +5,9 @@
  */
 package org.fcrepo.kernel.api.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This exception indicates that a resource could not be modified because it is currently being modified by another
  * transaction.
@@ -13,13 +16,37 @@ package org.fcrepo.kernel.api.exception;
  */
 public class ConcurrentUpdateException extends RepositoryRuntimeException {
 
+    private static final String LOG_MESSAGE =
+        "Cannot update %s because it is being updated by another transaction (%s).";
+    private static final String HTTP_MESSAGE = "Cannot update %s because it is being updated by another transaction";
+
+    private final String resource;
+    private final String existingTx;
+    private final String conflictingTx;
+
     /**
      * Constructor
      *
-     * @param msg message
+     * @param resource the Fedora response
+     * @param conflictingTx the transaction id attempting to lock the resource
+     * @param existingTx the transaction id holding the resource
      */
-    public ConcurrentUpdateException(final String msg) {
-        super(msg);
+    public ConcurrentUpdateException(final String resource, final String conflictingTx, final String existingTx) {
+        super(String.format(LOG_MESSAGE, resource, existingTx));
+        this.resource = resource;
+        this.conflictingTx = conflictingTx;
+        this.existingTx = existingTx;
+    }
+
+    public Map<String, String> getResponseBody(final boolean includeTxId) {
+        final var response = new HashMap<String, String>();
+        response.put("message", String.format(HTTP_MESSAGE, resource));
+        if (includeTxId) {
+            response.put("existingTransactionId", existingTx);
+            response.put("conflictingTransactionId", conflictingTx);
+        }
+
+        return response;
     }
 
 }
