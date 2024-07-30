805661ae862a4036b21e31c6d149b92eda96df0c
Dan Field <surfrdan@gmail.com>
2024-07-30 14:05:35 +0100

    Changing URL for repo registry (#2089)

--- fcrepo-configs/src/main/java/org/fcrepo/config/FedoraPropsConfig.java
+++ fcrepo-configs/src/main/java/org/fcrepo/config/FedoraPropsConfig.java
@@ -160,7 +160,7 @@ public class FedoraPropsConfig extends BasePropsConfig {
                 "on-going maintenance of the software and ensure Fedora is meeting the needs of the " +
                 "community, we are working to gather information on current installations. We strongly " +
                 "encourage all users to register their instance in the DuraSpace Community Supported " +
-                "Program Registry hosted by LYRASIS at https://registry.lyrasis.org/");
+                "Program Registry hosted by LYRASIS at https://fedora.lyrasis.org/register-your-site/");
         LOGGER.info("Fedora home: {}", fedoraHome);
         LOGGER.debug("Fedora home data: {}", fedoraData);
         try {
