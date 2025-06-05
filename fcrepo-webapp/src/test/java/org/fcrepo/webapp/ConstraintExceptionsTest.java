504f60b4d64734620261e812a6310b6b959b1f78
Ben Pennell <bbpennel@email.unc.edu>
2025-06-05 10:30:25 -0400

    Remove unneeded dependencies (#2226)
    * Remove Reflections and replace its only usage with spring
    
    * Remove spring aop and aspects, which we don't directly use
    
    * Remove old mockito and junit
    
    * Delete rdf for constraint exceptions that no longer exist
    
    * Remove site plugins
    
    * Remove htmlunit-core-js, which does not appear to be used in the one test that uses htmlunit

--- fcrepo-webapp/src/test/java/org/fcrepo/webapp/ConstraintExceptionsTest.java
+++ fcrepo-webapp/src/test/java/org/fcrepo/webapp/ConstraintExceptionsTest.java
@@ -12,7 +12,10 @@ import org.fcrepo.kernel.api.exception.ConstraintViolationException;
 import org.fcrepo.kernel.api.exception.RelaxableServerManagedPropertyException;
 
 import org.junit.jupiter.api.Test;
-import org.reflections.Reflections;
+import java.util.HashSet;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AssignableTypeFilter;
 
 import java.io.File;
 import java.util.Set;
@@ -25,9 +28,8 @@ public class ConstraintExceptionsTest {
 
     @Test
     public void testConstraintRdfExists() {
-        final Reflections reflections = new Reflections("org.fcrepo");
-        final Set<Class<? extends ConstraintViolationException>> subTypes =
-                reflections.getSubTypesOf(ConstraintViolationException.class);
+        final var subTypes = getConstraintViolationSubTypes();
+
         // Multiple is a wrapper to hold other constraint violations, it has no static file.
         subTypes.remove(MultipleConstraintViolationException.class);
         // Relaxable is a sub-type of ServerManagedPropertyException to specially handle relaxable properties, it has
@@ -35,10 +37,33 @@ public class ConstraintExceptionsTest {
         subTypes.remove(RelaxableServerManagedPropertyException.class);
         subTypes.add(ConstraintViolationException.class);
 
+        assertTrue(subTypes.size() > 1, "Must be more than one subtype of ConstraintViolationException");
+
         for (final Class<? extends ConstraintViolationException> c : subTypes) {
             final File file = new File("src/main/webapp/static/constraints/" + c.getSimpleName() + ".rdf");
             assertTrue(file.exists(), "Expected to find: " + file.getPath());
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    private Set<Class<? extends ConstraintViolationException>> getConstraintViolationSubTypes() {
+        final var provider =
+                new ClassPathScanningCandidateComponentProvider(false);
+        provider.addIncludeFilter(new AssignableTypeFilter(ConstraintViolationException.class));
+
+        final var components = provider.findCandidateComponents("org.fcrepo");
+        final Set<Class<? extends ConstraintViolationException>> subTypes = new HashSet<>();
+        for (final BeanDefinition component : components) {
+            try {
+                final var clazz = Class.forName(component.getBeanClassName());
+                if (ConstraintViolationException.class.isAssignableFrom(clazz)) {
+                    subTypes.add((Class<? extends ConstraintViolationException>) clazz);
+                }
+            } catch (ClassNotFoundException e) {
+                // Skip if class can't be loaded
+            }
+        }
+        return subTypes;
     }
 
 }
